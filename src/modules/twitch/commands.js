0696943590988788fa2434eaba7531b6212dc0a0
newmediapilot <marcin@newmediapilot.com>
2024-11-27 19:23:20 -0500

    Update commands.js

--- src/modules/twitch/commands.js
+++ src/modules/twitch/commands.js
@@ -76,7 +76,7 @@ async function parseCommand(channel, tags, message) {
 
   if (currentCommand === COMMANDS.heart) {
     const heartRate = getParam("sensor_heart_rate");
-    await twitchMessageCreate(`💜 Heart rate is ${heartRate}`);
+    await twitchMessageCreate(`🤖 💜 Heart rate is ${heartRate}`);
     return true;
   }
 
