6128c0fd0bd77ff84a98f68182ba911c3ceb2bfb
newmediapilot <marcin@newmediapilot.com>
2024-11-28 00:38:03 -0500

    add signal dashboard

--- src/modules/public/signal.js
+++ src/modules/public/signal.js
@@ -8,44 +8,40 @@
  *   @param {object} res - The HTTP response object used to send the response.
  */
 
-const { setParam } = require("../store/manager");
-const { twitchMarkerCreate } = require("../twitch/marker");
-const { twitchMessageCreate } = require("../twitch/message");
-const { getHeartRateMessage } = require("../sensor/listen");
-
-const PUBLIC_SIGNALS = [
-  { type: "mark", description: "game:start", label: "Mark: Game Start" },
-  { type: "heart", description: "heart", label: "Say Heart Rate" },
-];
-
-setParam("public_signals", PUBLIC_SIGNALS);
+const {setParam} = require("../store/manager");
+const {twitchMarkerCreate} = require("../twitch/marker");
+const {twitchMessageCreate} = require("../twitch/message");
+const {getHeartRateMessage} = require("../sensor/listen");
 
 let isCreating = false;
 
 async function signalCreate(req, res) {
-  if (isCreating) return res.status(400).send("Busy");
-  isCreating = true;
 
-  const type = req.query.type;
-  const description = req.query.description;
-
-  if (!description || !type) {
-    return res.status(400).send("Query missing");
-  }
-
-  try {
-    if ("mark" === type) {
-      await twitchMarkerCreate(description);
+    if (isCreating) {
+        return res.status(400).send("Busy");
     }
-    if ("heart" === type) {
-      await twitchMessageCreate(getHeartRateMessage());
+
+    isCreating = true;
+    const type = req.query.type;
+    const description = req.query.description;
+
+    if (!description || !type) {
+        return res.status(400).send("Query missing");
     }
-    isCreating = false;
-    return res.send("Success");
-  } catch (error) {
-    isCreating = false;
-    return res.status(400).send("Error" + error);
-  }
+
+    try {
+        if ("mark" === type) {
+            await twitchMarkerCreate(description);
+        }
+        if ("heart" === type) {
+            await twitchMessageCreate(getHeartRateMessage());
+        }
+        isCreating = false;
+        return res.send("Success");
+    } catch (error) {
+        isCreating = false;
+        return res.status(400).send("Error" + error);
+    }
 }
 
-module.exports = { signalCreate };
+module.exports = {signalCreate};
