745a22b65ff85d9b574628606b4e9bd211f655e1
newmediapilot <marcin@newmediapilot.com>
2024-11-29 13:59:49 -0500

    Update send_signal.js

--- src/assets/js/send_signal.js
+++ src/assets/js/send_signal.js
@@ -2,36 +2,37 @@ const sendSignal = (el) => {
 
     const initialText = el.innerText;
     const waitDefault = 3000;
-    const waitInput = el.getAttribute('data-wait') || waitDefault;
     const sendingText = el.getAttribute('data-sending');
     const errorText = el.getAttribute('data-error');
     const successText = el.getAttribute('data-success');
 
-    const setState = (label, disabled, state) => {
+    const setState = (label, isDisabled, state) => {
         el.innerText = label;
-        el.disabled = disabled;
+        el.disabled = isDisabled;
         el.setAttribute('data-state', state);
     };
 
     setState(sendingText, true, 'loading');
-
     const getRes = axios.get(el.getAttribute('data-href'));
 
     getRes.then(getRes => {
         console.log('Success:', getRes);
         setState(successText, true, 'success');
-        setTimeout(() => setState(initialText, true, 'idle'), waitInput);
+        setTimeout(() => setState(initialText, false, 'idle'), waitDefault);
     });
 
     getRes.catch(error => {
         console.log('Error:', error);
-        // showDialog('generic_dialog', 'Oops', `Looks like we had an error ${error}`, 'Continue');
         setState(errorText, true, 'error');
-        setTimeout(() => setState(initialText, true, 'idle'), waitDefault);
-        el.blur();
+        setTimeout(() => setState(initialText, false, 'idle'), waitDefault);
     });
 
     getRes.finally(() => {
         console.info('Complete.');
+        setTimeout(() => {
+            setState(initialText, false, 'idle');
+            el.blur();
+        }, waitDefault);
     });
+
 };
