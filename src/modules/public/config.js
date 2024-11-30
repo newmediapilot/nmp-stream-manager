0a7d5ad1fb5743138ecd0987b462d6232d09a305
newmediapilot <marcin@newmediapilot.com>
2024-11-29 20:57:36 -0500

    adding config save features

--- src/modules/public/config.js
+++ src/modules/public/config.js
@@ -1,98 +1,107 @@
-const signals = [
-    {
-        theme: "orange",
-        type: "ad",
-        description: "30",
-        wait: "30000",
-        emoji: "📢", // Works for Ad Start
-        label: "Start Ad (30s)",
-    },
-    {
-        theme: "orange",
-        type: "ad",
-        description: "60",
-        wait: "60000",
-        emoji: "📢", // Works for Ad Start
-        label: "Start Ad (60s)",
-    },
-    {
-        theme: "orange",
-        type: "ad",
-        description: "120",
-        wait: "120000",
-        emoji: "📢", // Works for Ad Start
-        label: "Start Ad (2min)",
-    },
-    {
-        theme: "orange",
-        type: "ad",
-        description: "180",
-        wait: "180000",
-        emoji: "📢", // Works for Ad Start
-        label: "Start Ad (3min)",
-    },
-    {
-        theme: "aqua",
-        type: "heart",
-        description: "heart",
-        wait: "3000",
-        emoji: "❤️", // More visually consistent for Heart Rate
-        label: "Heart Rate",
-    },
-    {
-        theme: "purple",
-        type: "mark",
-        description: "match:start",
-        wait: "3000",
-        emoji: "⏰", // Clock represents "Start"
-        label: "Start",
-    },
-    {
-        theme: "pink",
-        type: "mark",
-        description: "match:fight",
-        wait: "3000",
-        emoji: "🥊", // Fits "Fight" perfectly
-        label: "Fight",
-    },
-    {
-        theme: "deepyellow",
-        type: "mark",
-        description: "match:checkpoint",
-        wait: "3000",
-        emoji: "👀", // Eye represents "Look"
-        label: "Look",
-    },
-    {
-        theme: "deepyellow",
-        type: "mark",
-        description: "match:idle",
-        wait: "3000",
-        emoji: "🎬", // Clapperboard for "Clip"
-        label: "Clip",
-    },
-    {
-        theme: "red",
-        type: "mark",
-        description: "match:complete",
-        wait: "3000",
-        emoji: "👾", // Skull for "Game Over"
-        label: "Game Over",
-    },
-];
-
-const putDashboardConfig = () => {
-
+const CONFIGS = {
+    signals: [
+        {
+            theme: "orange",
+            type: "ad",
+            description: "30",
+            wait: "30000",
+            emoji: "📢", // Works for Ad Start
+            label: "Start Ad (30s)",
+        },
+        {
+            theme: "orange",
+            type: "ad",
+            description: "60",
+            wait: "60000",
+            emoji: "📢", // Works for Ad Start
+            label: "Start Ad (60s)",
+        },
+        {
+            theme: "orange",
+            type: "ad",
+            description: "120",
+            wait: "120000",
+            emoji: "📢", // Works for Ad Start
+            label: "Start Ad (2min)",
+        },
+        {
+            theme: "orange",
+            type: "ad",
+            description: "180",
+            wait: "180000",
+            emoji: "📢", // Works for Ad Start
+            label: "Start Ad (3min)",
+        },
+        {
+            theme: "aqua",
+            type: "heart",
+            description: "heart",
+            wait: "3000",
+            emoji: "❤️", // More visually consistent for Heart Rate
+            label: "Heart Rate",
+        },
+        {
+            theme: "purple",
+            type: "mark",
+            description: "match:start",
+            wait: "3000",
+            emoji: "⏰", // Clock represents "Start"
+            label: "Start",
+        },
+        {
+            theme: "pink",
+            type: "mark",
+            description: "match:fight",
+            wait: "3000",
+            emoji: "🥊", // Fits "Fight" perfectly
+            label: "Fight",
+        },
+        {
+            theme: "deepyellow",
+            type: "mark",
+            description: "match:checkpoint",
+            wait: "3000",
+            emoji: "👀", // Eye represents "Look"
+            label: "Look",
+        },
+        {
+            theme: "deepyellow",
+            type: "mark",
+            description: "match:idle",
+            wait: "3000",
+            emoji: "🎬", // Clapperboard for "Clip"
+            label: "Clip",
+        },
+        {
+            theme: "red",
+            type: "mark",
+            description: "match:complete",
+            wait: "3000",
+            emoji: "👾", // Skull for "Game Over"
+            label: "Game Over",
+        },
+    ]
 };
 
-const getDashboardConfig = () => {
+const putConfig = (key) => {
+    if(!Object.keys(CONFIGS).includes(key)){
+        // error key incorrect
+    }
+    const fileName = `.${key}`;
+    return true;
+};
 
-
-
-    return signals;
+const getConfig = (key) => {
+    if(!Object.keys(CONFIGS).includes(key)){
+        // error key incorrect
+    }
+    const fileName = `.${key}`;
+    // fs load sync
+    CONFIGS[key] = JSON.parse(config);
+    return CONFIGS[key];
 };
 
 module.exports = {
-    putDashboardConfig,
-    getDashboardConfig,
+    putConfig,
+    getConfig,
 };
