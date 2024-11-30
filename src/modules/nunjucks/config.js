e05e40376829f90e90e7b52c63594530dec97a9d
newmediapilot <marcin@newmediapilot.com>
2024-11-30 02:53:58 -0500

    Update config.js

--- src/modules/nunjucks/config.js
+++ src/modules/nunjucks/config.js
@@ -11,7 +11,7 @@ const { getParam, getAllParams } = require("../store/manager");
  * Configures Nunjucks for the provided Express app.
  * @param {object} app - The Express app instance.
  */
-function configureNunjucks(app) {
+const configureNunjucks = (app) => {
   const nunjucksEnv = nunjucks.configure("src/views", {
     autoescape: true,
     express: app,
@@ -24,6 +24,8 @@ function configureNunjucks(app) {
   app.set("view engine", "html");
 
   app.use(express.static("src/assets"));
-}
 
-module.exports = { configureNunjucks };
+  appRef = app;
+};
+
+module.exports = { configureNunjucks, renderNunjucks };
