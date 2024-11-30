f2df46550f069053a084dc276007dbd77638ec54
newmediapilot <marcin@newmediapilot.com>
2024-11-30 15:50:49 -0500

    prettier only

--- src/modules/public/config.js
+++ src/modules/public/config.js
@@ -2,20 +2,20 @@
 const PUBLIC_CONFIGS = {
   signals: [
     {
-      theme: 'orange',
-      type: 'ad',
-      description: '30',
-      wait: '30000',
-      emoji: '📢',
-      label: 'Start Ad (30s)',
+      theme: "orange",
+      type: "ad",
+      description: "30",
+      wait: "30000",
+      emoji: "📢",
+      label: "Start Ad (30s)",
     },
     {
-      theme: 'orange',
-      type: 'ad',
-      description: '60',
-      wait: '60000',
-      emoji: '📢',
-      label: 'Start Ad (60s)',
+      theme: "orange",
+      type: "ad",
+      description: "60",
+      wait: "60000",
+      emoji: "📢",
+      label: "Start Ad (60s)",
     },
     // {
     //   theme: 'orange',
@@ -84,47 +84,52 @@ const PUBLIC_CONFIGS = {
   ],
 };
 
-const fs = require('fs');
-const path = require('path');
-const {setParam, getParam} = require('../store/manager');
+const fs = require("fs");
+const path = require("path");
+const { setParam, getParam } = require("../store/manager");
 
 // Initialize config in memory for template
 // IF we never had a file create one,
 // IF we have a file use that data
 const initializePublicConfigs = async (type) => {
-  console.log('initializePublicConfigs :: start');
+  console.log2(process.cwd(),"initializePublicConfigs :: start");
   const fileName = path.resolve(`.${type}.json`);
   try {
     if (!fs.existsSync(fileName)) {
       setParam("dashboard_signals_config", PUBLIC_CONFIGS.signals);
       putConfig("signals", PUBLIC_CONFIGS.signals);
-      console.log('initializePublicConfigs :: new file');
+      console.log2(process.cwd(),"initializePublicConfigs :: new file");
     } else {
       setParam("dashboard_signals_config", getConfig("signals"));
-      console.log('initializePublicConfigs :: load file');
+      console.log2(process.cwd(),"initializePublicConfigs :: load file");
     }
-    console.log('initializePublicConfigs :: success');
+    console.log2(process.cwd(),"initializePublicConfigs :: success");
     return true;
   } catch (e) {
-    console.log('initializePublicConfigs :: error', e);
+    console.err2(process.cwd(),"initializePublicConfigs :: error", e);
     return false;
   } finally {
-    console.log('initializePublicConfigs :: done');
+    console.log2(process.cwd(),"initializePublicConfigs :: done");
   }
 };
 
 // Save configuration to a file (synchronous)
 const putConfig = (type, config) => {
   const fileName = path.resolve(`.${type}.json`);
-  console.log('putConfig :: file:', fileName, ':: contents :', config.map(c=>c.label));
+  console.log(
+    "putConfig :: file:",
+    fileName,
+    ":: contents :",
+    config.map((c) => c.label),
+  );
   fs.writeFileSync(fileName, JSON.stringify(config, null, 2));
 };
 
 // Load configuration from a file (synchronous)
 const getConfig = (type) => {
   const fileName = path.resolve(`.${type}.json`);
-  console.log('getConfig :: file:', fileName);
-  return JSON.parse(fs.readFileSync(fileName, 'utf-8'));
+  console.log2(process.cwd(),"getConfig :: file:", fileName);
+  return JSON.parse(fs.readFileSync(fileName, "utf-8"));
 };
 
 // Parse updates and apply the swaps to the written file
@@ -134,7 +139,7 @@ const applySignalsPayload = (payloadJSON) => {
   const signalsOrigin = getParam("dashboard_signals_config").slice();
   const signalsTarget = signalsOrigin.slice();
 
-  while (payloadAction.length>0) {
+  while (payloadAction.length > 0) {
     const [from, to] = payloadAction.splice(0, 2);
     const actionFrom = JSON.parse(JSON.stringify(signalsOrigin[from]));
     const actionTo = JSON.parse(JSON.stringify(signalsOrigin[to]));
@@ -142,11 +147,17 @@ const applySignalsPayload = (payloadJSON) => {
     signalsTarget[to] = actionFrom;
   }
 
-  console.log('applySignalsPayload :: signalsOrigin ::', signalsOrigin.map(t=>t.label));
-  console.log('applySignalsPayload :: signalsTarget ::', signalsTarget.map(t=>t.label));
+  console.log(
+    "applySignalsPayload :: signalsOrigin ::",
+    signalsOrigin.map((t) => t.label),
+  );
+  console.log(
+    "applySignalsPayload :: signalsTarget ::",
+    signalsTarget.map((t) => t.label),
+  );
 
   // Save to memory
-  setParam("dashboard_signals_config",signalsTarget);
+  setParam("dashboard_signals_config", signalsTarget);
 
   return signalsTarget;
 };
@@ -156,23 +167,21 @@ const applySignalsPayload = (payloadJSON) => {
 const publicConfigUpdate = (req, res) => {
   const { type, payload } = req.query;
 
-  if (!PUBLIC_CONFIGS[type]){
-    return res.status(400).json({ error: 'Invalid type: ' + type + '' });
+  if (!PUBLIC_CONFIGS[type]) {
+    return res.status(400).json({ error: "Invalid type: " + type + "" });
   }
 
   try {
-    if (type === 'signals') {
-      putConfig('signals', applySignalsPayload(payload));
+    if (type === "signals") {
+      putConfig("signals", applySignalsPayload(payload));
     }
-    res
-      .status(200)
-      .json({
-        message: 'Configuration for ' + type + ' updated successfully.',
-      });
+    res.status(200).json({
+      message: "Configuration for " + type + " updated successfully.",
+    });
   } catch (error) {
     console.err2(
       process.cwd(),
-      'Error processing configuration:',
+      "Error processing configuration:",
       error.message,
     );
     res.status(500).json({ error: error.message });
