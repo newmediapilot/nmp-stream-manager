f5a382f7a894ba759e02f657dd5b9d2a4bd83a92
newmediapilot <marcin@newmediapilot.com>
2024-12-02 02:48:42 -0500

    Update generic_dialog.js

--- src/assets/script/generic_dialog.js
+++ src/assets/script/generic_dialog.js
@@ -1,6 +1,6 @@
 function showDialog(id, title, body) {
   const dialog = document.getElementById(id);
-  document.getElementById("menu-toggle").checked= false;
+  document.querySelector("#menu-toggle").checked= false;
   dialog.querySelector(".dialog--title").innerText = title;
   dialog.querySelector(".dialog--body").innerText = body;
   dialog.showModal();
