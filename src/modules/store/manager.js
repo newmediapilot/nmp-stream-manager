7fb65c6184feb1a02eadbda47733c38d07333235
newmediapilot <marcin@newmediapilot.com>
2024-12-04 07:39:29 -0500

    Update manager.js

--- src/modules/store/manager.js
+++ src/modules/store/manager.js
@@ -75,7 +75,7 @@ function getParam(key) {
       value,
     );
   } else {
-    console.info2(process.cwd(), "Get", key, "::", value);
+    // console.info2(process.cwd(), "Get", key, "::", value);
   }
   return value;
 }
