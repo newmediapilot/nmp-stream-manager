66b657e3ac9b53ec90c0e8cf745754c9462009cf
newmediapilot <marcin@newmediapilot.com>
2024-12-04 18:31:38 -0500

    Update send_signal.js

--- src/assets/script/send_signal.js
+++ src/assets/script/send_signal.js
@@ -8,7 +8,7 @@ const sendSignal = (el) => {
   const successText = el.getAttribute("data-success");
 
   const setState = (label, isDisabled, state) => {
-    el.innerText = label;
+    el.querySelector('div').innerHTML = label;
     el.disabled = isDisabled;
     el.setAttribute("data-state", state);
   };
