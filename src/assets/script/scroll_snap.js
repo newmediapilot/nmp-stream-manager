0df32a9f033e4bdc36ca83c5d0af68662a1fd76f
newmediapilot <marcin@newmediapilot.com>
2024-12-04 20:44:45 -0500

    scrollsnap added 2

---
+++ src/assets/script/scroll_snap.js
@@ -0,0 +1,23 @@
+const scrollSnap = (elQueryString) => {
+    let to = null;
+    let anim = null;
+    let duration = 333;
+    let containerEl = document.querySelector(elQueryString);
+    const height = containerEl.children[0].getBoundingClientRect().height;
+    const scrollFunct = () => {
+        to && clearTimeout(to);
+        to = setTimeout(() => {
+            const scrollTop = Math.round(containerEl.scrollTop / height) * height;
+            anim && anime.remove('.dashboard');
+            anim = anime({
+                scrollTop,
+                duration,
+                targets: elQueryString,
+                easing: 'easeInOutQuad'
+            });
+        }, duration);
+    };
+    document.documentElement.addEventListener('touchend', scrollFunct);
+    document.documentElement.addEventListener('mouseup', scrollFunct);
+    document.documentElement.addEventListener('wheel', scrollFunct);
+};
