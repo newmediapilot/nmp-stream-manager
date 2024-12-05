8cb7a5f207591c613047c495bf7f482ddcf0e12b
newmediapilot <marcin@newmediapilot.com>
2024-12-04 20:47:08 -0500

    Update scroll_snap.js

--- src/assets/script/scroll_snap.js
+++ src/assets/script/scroll_snap.js
@@ -1,7 +1,7 @@
 const scrollSnap = (elQueryString) => {
     let to = null;
     let anim = null;
-    let duration = 333;
+    let duration = 1000;
     let containerEl = document.querySelector(elQueryString);
     const height = containerEl.children[0].getBoundingClientRect().height;
     const scrollFunct = () => {
@@ -11,12 +11,14 @@ const scrollSnap = (elQueryString) => {
             anim && anime.remove('.dashboard');
             anim = anime({
                 scrollTop,
-                duration,
+                duration:100,
                 targets: elQueryString,
                 easing: 'easeInOutQuad'
             });
         }, duration);
     };
+    document.documentElement.addEventListener('touchstart', ()=>clearTimeout(to));
+    document.documentElement.addEventListener('mousedown', ()=>clearTimeout(to));
     document.documentElement.addEventListener('touchend', scrollFunct);
     document.documentElement.addEventListener('mouseup', scrollFunct);
     document.documentElement.addEventListener('wheel', scrollFunct);
