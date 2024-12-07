5aaf7f515ac2b470b052ddad4506129b23c450e4
newmediapilot <marcin@newmediapilot.com>
2024-12-07 12:52:38 -0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,23 @@
  */
 
 const ROUTES = {
-  // Twitch API
-  TWITCH_LOGIN: "/twitch/login",
-  TWITCH_LOGIN_SUCCESS: "/t/w/i/t/c/h/l/o/g/i/n/s/u/c/c/e/s/s/",
-  TWITCH_REDIRECT_URL: process.env.TWITCH_REDIRECT_URL,
-  // Public HTML
-  PUBLIC_INDEX: "/",
-  PUBLIC_DASHBOARD: "/public/dashboard",
-  PUBLIC_SETTINGS: "/public/settings",
-  PUBLIC_MODULES: "/public/modules",
-  PUBLIC_FEATURE_EMBED: "/public/feature/embed",
-  PUBLIC_HEART_EMBED: "/public/heart/embed",
-  PUBLIC_HEART_PING: "/public/heart/ping",
-  PUBLIC_SIGNAL_CREATE: "/public/signal/create",
-  PUBLIC_CONFIG_UPDATE: "/public/config/update",
-  PUBLIC_STYLE_UPDATE: "/public/style/update",
+    // Twitch API
+    TWITCH_LOGIN: "/twitch/login",
+    TWITCH_LOGIN_SUCCESS: "/t/w/i/t/c/h/l/o/g/i/n/s/u/c/c/e/s/s/",
+    TWITCH_REDIRECT_URL: process.env.TWITCH_REDIRECT_URL,
+    // Public HTML
+    PUBLIC_INDEX: "/",
+    PUBLIC_DASHBOARD: "/public/dashboard",
+    PUBLIC_SETTINGS: "/public/settings",
+    PUBLIC_MODULES: "/public/modules",
+    // Embed Panels
+    PUBLIC_FEATURE_EMBED: "/public/feature/embed",
+    PUBLIC_HEART_EMBED: "/public/heart/embed",
+    // Api Calls
+    PUBLIC_HEART_PING: "/public/heart/ping",
+    PUBLIC_SIGNAL_CREATE: "/public/signal/create",
+    PUBLIC_CONFIG_UPDATE: "/public/config/update",
+    PUBLIC_STYLE_UPDATE: "/public/style/update",
 };
 
 module.exports = ROUTES;
