d18f2f00843f4bd28b804bc8dfe622ca39552b12
newmediapilot <marcin@newmediapilot.com>
2024-12-07 14:04:56 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,36 +6,32 @@
 require("dotenv").config();
 const ROUTES = require("./routes");
 const express = require("express");
-const { requestLogger } = require("./logger");
-const { startServices } = require("./modules/start");
-const { publicSignalCreate } = require("./modules/public/signal");
-const { publicConfigUpdate } = require("./modules/public/config");
-const { publicStyleUpdate } = require("./modules/public/style");
-const { publicHeartPing } = require("./modules/sensor/ping");
-const { twitchLogin, twitchLoginSuccess } = require("./modules/twitch/login");
-
+const {requestLogger} = require("./logger");
+const {startServices} = require("./modules/start");
+const {publicSignalCreate} = require("./modules/public/signal");
+const {publicConfigUpdate} = require("./modules/public/config");
+const {publicStyleUpdate} = require("./modules/public/style");
+const {publicHeartPing} = require("./modules/sensor/ping");
+const {twitchLogin, twitchLoginSuccess} = require("./modules/twitch/login");
 const app = express();
-
 // Just log
 app.use(requestLogger);
-
 // Twitch API Start
 app.all(ROUTES.TWITCH_LOGIN, twitchLogin);
 app.all(ROUTES.TWITCH_LOGIN_SUCCESS, twitchLoginSuccess);
-
 // Public HTML
 app.all(ROUTES.PUBLIC_INDEX, (req, res) => res.render("index"));
 app.all(ROUTES.PUBLIC_DASHBOARD, (req, res) => res.render("dashboard"));
 app.all(ROUTES.PUBLIC_SETTINGS, (req, res) => res.render("settings"));
 app.all(ROUTES.PUBLIC_MODULES, (req, res) => res.render("modules"));
+// Embeds
 app.all(ROUTES.PUBLIC_FEATURE_EMBED, (req, res) => res.render("embed/iframe-twitch"));
-app.all(ROUTES.PUBLIC_HEART_EMBED, (req, res) => res.render("embed/iframe-twitch"));
+app.all(ROUTES.PUBLIC_HEART_EMBED, (req, res) => res.render("embed/iframe-heart"));
+// Api
 app.all(ROUTES.PUBLIC_SIGNAL_CREATE, publicSignalCreate);
 app.all(ROUTES.PUBLIC_CONFIG_UPDATE, publicConfigUpdate);
 app.all(ROUTES.PUBLIC_STYLE_UPDATE, publicStyleUpdate);
-
 // Pings
 app.all(ROUTES.PUBLIC_HEART_PING, publicHeartPing);
-
 // Init
 startServices(app);
