6a7f25c4d0d76a42e698e266f5d76aae890cfec5
newmediapilot <marcin@newmediapilot.com>
2024-12-07 14:31:01 -0500

    Update start.sh

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-bash .deploy/get-node.sh
+# Directory context
+SCRIPT_DIR=$(dirname "$0")
+chmod +x "$SCRIPT_DIR"/*.sh
+bash "$SCRIPT_DIR/get-node.sh"
+bash "$SCRIPT_DIR/get-bpm.sh"
+
+# Start app
+NODE_BIN="$SCRIPT_DIR/../.bin/node/node.exe"
+INDEX_FILE="$SCRIPT_DIR/../src/index.js"
+"$NODE_BIN" "$INDEX_FILE"
