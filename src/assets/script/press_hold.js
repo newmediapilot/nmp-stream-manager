5d17c9389cd9d0e2c0bd5bc0d927fc680863c77e
newmediapilot <marcin@newmediapilot.com>
2024-12-07 01:57:20 -0500

    Create press_hold.js

---
+++ src/assets/script/press_hold.js
@@ -0,0 +1,26 @@
+const detectPressHold = (onHoldDetected, onHoldReleased) => {
+    let isHeldDown = false;
+    let timeoutId = null;
+    const debounceTime = 500;
+
+    const onDown = () => {
+        if (!isHeldDown) {
+            isHeldDown = true;
+        }
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            onHoldDetected();
+        }, debounceTime);
+    };
+
+    const onUp = () => {
+        isHeldDown = false;
+        onHoldReleased();
+        clearTimeout(timeoutId);
+    };
+
+    document.addEventListener('mousedown', onDown);
+    document.addEventListener('touchstart', onDown);
+    document.addEventListener('mouseup', onUp);
+    document.addEventListener('touchend', onUp);
+};
