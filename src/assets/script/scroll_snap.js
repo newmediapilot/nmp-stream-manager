88b68a85347bbe1c14539aaa16b9b17b4ab3bd39
newmediapilot <marcin@newmediapilot.com>
2024-12-08 04:07:23 -0500

    Update scroll_snap.js

--- src/assets/script/scroll_snap.js
+++ src/assets/script/scroll_snap.js
@@ -1,13 +1,18 @@
 const scrollSnap = (elQueryString) => {
 
     let containerEl = document.querySelector(elQueryString);
+    let to = null;
+
+    const scrollFunct = () => {
+        console.log('containerEl', containerEl);
+    };
 
     // TODO: let's make things scroll-snap at some point!
-    // document.addEventListener('touchstart', () => clearTimeout(to));
-    // document.addEventListener('mousedown', () => clearTimeout(to));
-    // document.addEventListener('touchend', () => scrollFunct());
-    // document.addEventListener('mouseup', () => scrollFunct());
-    // document.addEventListener('wheel', () => scrollFunct());
+    document.addEventListener('touchstart', () => clearTimeout(to));
+    document.addEventListener('mousedown', () => clearTimeout(to));
+    document.addEventListener('touchend', () => scrollFunct(e));
+    document.addEventListener('mouseup', () => scrollFunct());
+    document.addEventListener('wheel', () => scrollFunct());
 
     // Anchor remembers with debounce
     if (
