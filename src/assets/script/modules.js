ef183e5e4e926dfc97d388b78f515a685a26bfb0
newmediapilot <marcin@newmediapilot.com>
2024-12-07 22:31:59 -0500

    Update modules.js

--- src/assets/script/modules.js
+++ src/assets/script/modules.js
@@ -1,6 +1,15 @@
 // Pushes the contents of all variables across all modules to write out
 const pushStyleUpdates = () => {
-    const payload = Array.from(document.querySelectorAll('input[type="radio"]:checked')).map(el => el.value).join("");
+    // Assign attributes into a --variable format
+    const payload = Array.from(document.querySelectorAll('input[type="range"]')).map(
+        (el) => {
+            return [
+                el.getAttribute('id'),
+                el.value,
+                el.getAttribute('name')
+            ].join("")
+        }
+    ).join("");
     document.querySelector('#public_module_styles').innerHTML = payload;
     console.log('pushStyleUpdates::payload', payload);
     payload && axios.get("/public/style/update", {
@@ -8,18 +17,17 @@ const pushStyleUpdates = () => {
             type: "style",
             payload
         },
-    })
-        .finally(() => {
-            socketEmitReload();
-            console.log('pushStyleUpdates::socketEmitReload');
-        });
+    }).finally(() => {
+        socketEmitReload();
+        console.log('pushStyleUpdates::socketEmitReload');
+    });
+    applyStyleUpdates();
 };
 
 // Reads checkboxes and sets checked based on whether css contains their value
 const applyStyleUpdates = () => {
-    const radioEls = Array.from(document.querySelectorAll('input[type="radio"]'));
     const styleString = document.querySelector('#public_module_styles');
-    radioEls.forEach(el => {
+    Array.from(document.querySelectorAll('input[type="range"]')).forEach(el => {
         el.checked = styleString.innerText.includes(el.value);
     });
 };
@@ -36,17 +44,4 @@ const showPanelQR = (dialogQuerySelector, type) => {
         URL,
         dialogQuerySelectorCanvas
     );
-};
-
-// Toggles panels via checkbox
-const togglePanelPreview = (contextEl) => {
-    const contentHeight = getComputedStyle(document.documentElement).getPropertyValue('--js-module-content-height');
-    const titleHeight = getComputedStyle(document.documentElement).getPropertyValue('--js-module-title-height');
-    const checkboxEls = Array.from(document.querySelectorAll('.modules input[type="checkbox"]'));
-    console.log('togglePanelPreview', contentHeight, titleHeight);
-    checkboxEls.forEach((el,index) => {
-        const moduleEl = document.querySelectorAll('.module')[index];
-        el.checked = contextEl === el;
-        moduleEl.style.height = (el.checked ? contentHeight : titleHeight);
-    });
 };
