fc8067e0663832578bef64074a67013d4ca25d19
newmediapilot <marcin@newmediapilot.com>
2024-12-07 20:07:30 -0500

    Update router.js

--- src/assets/script/router.js
+++ src/assets/script/router.js
@@ -1,4 +1,6 @@
 const getURL = (path) => {
+    // TODO: implement the real thing
+    document.querySelector('#menu-toggle').checked = false;
     if (window.self !== window.top) {
         window.parent.postMessage(
             {type: 'path', message: path},
