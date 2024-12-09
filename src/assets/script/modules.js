771790309e157a46b9ba46bf698426fdd2e67a7e
newmediapilot <marcin@newmediapilot.com>
2024-12-09 08:14:50 -0500

    restore works a bit

--- src/assets/script/modules.js
+++ src/assets/script/modules.js
@@ -1,38 +1,44 @@
-// Simulates a joystick
-const renderMatrixStyle = () => {
-    const updateInputValues = () => {
-        Array.from(document.querySelectorAll('.controls label')).forEach((label) => {
-            const {width, height} = label.getBoundingClientRect();
-            const lx = label.scrollLeft;
-            const ly = label.scrollTop;
-            const percentageX = lx / (width / 2);
-            const percentageY = ly / (height / 2);
-            const inputX = label.children[0];
-            const inputY = label.children[1];
-            inputX.value = inputX.max * percentageX;
-            inputY.value = inputY.max * percentageY;
+// Reads styles back and re-applies to interface
+const applyStyleUpdates = () => {
+    Array.from(document.querySelectorAll('.controls label'), (label) => {
+        const payload = document.querySelector('#public_module_styles').innerHTML;
+        const formX = label.children[0];
+        const formY = label.children[1];
+        const nameX = formX.name;
+        const nameY = formY.name;
+        if (1 === payload.split(nameX).length) return;// must contain 'name' to be read
+        const valueX = payload
+            .split(nameX)[1]
+            .split(':')[1]
+            .split(';')[0];
+        const valueY = payload
+            .split(nameY)[1]
+            .split(':')[1]
+            .split(';')[0];
+        const scrollTopPercent = (valueX - formX.min) / (formX.max - formX.min);
+        const scrollLeftPercent = (valueY - formY.min) / (formY.max - formY.min);
+        const {width, height} = label.getBoundingClientRect();
+        const top = (scrollTopPercent) * (height);
+        const left = (scrollLeftPercent) * (width);
+        label.scrollTo({
+            top,
+            left,
         });
-    };
-    document.addEventListener('touchmove', (e) => {
-        updateInputValues();
-    });
-    document.addEventListener('mousemove', (e) => {
-        updateInputValues();
-    });
-    document.addEventListener('touchend', (e) => {
-        updateInputValues();
-        pushStyleUpdates();
-        renderDynamicStyles();
-    });
-    document.addEventListener('mouseup', (e) => {
-        updateInputValues();
-        pushStyleUpdates();
-        renderDynamicStyles();
     });
 };
-
-// Realtime write of collected data into <style>
-const renderDynamicStyles = () => {
+// Gathers scrollbar coordinates into forms and also iframe
+const storeInputValues = () => {
+    Array.from(document.querySelectorAll('.controls label')).forEach((label) => {
+        const {width, height} = label.getBoundingClientRect();
+        const lx = label.scrollLeft;
+        const ly = label.scrollTop;
+        const valueX = lx / (width / 2);
+        const valueY = ly / (height / 2);
+        const inputX = label.children[0];
+        const inputY = label.children[1];
+        inputX.value = inputX.max * valueX;
+        inputY.value = inputY.max * valueY;
+    });
     const payload = Array.from(document.querySelectorAll('input[type="range"]')).map(
         (el) => {
             return `${el.name}:${el.value}`;
@@ -44,59 +50,47 @@ const renderDynamicStyles = () => {
             el.innerHTML = `:root{${payload};}`
         })
     });
-    // console.log('renderDynamicStyles');
-    return payload;
+    console.log('storeInputValues', payload);
+};
+// Simulates a joystick
+const initializeInputForms = () => {
+    const updateValues = () => {
+        storeInputValues();
+    };
+    document.addEventListener('mousedown', (e) => {
+        document.addEventListener('mousemove', updateValues);
+    });
+    document.addEventListener('mouseup', (e) => {
+        document.removeEventListener('mousemove', updateValues);
+        storeInputValues();
+        sendInputValues();
+    });
+    document.addEventListener('touchmove', (e) => {
+        storeInputValues();
+    });
+    document.addEventListener('touchend', (e) => {
+        storeInputValues();
+        sendInputValues();
+    });
 };
 
 // Pushes the contents of all variables across all modules to write out
-const pushStyleUpdates = () => {
-    const payload = renderDynamicStyles();
+const sendInputValues = () => {
+    let payload = document.querySelector('#public_module_styles')
+        .innerHTML
+        .replace(":root{", "")
+        .replace(";}", "");
+    // Send it
     payload && axios.get("/public/style/update", {
         params: {
             type: "style",
             payload
         },
     }).finally(() => {
+        storeInputValues();
         socketEmitReload();
-        renderDynamicStyles();
-        applyStyleUpdates();
-    });
-    console.log('pushStyleUpdates', payload);
-};
-
-// Reads styles back and re-applies to interface
-const applyStyleUpdates = () => {
-    const payload = document.querySelector('#public_module_styles').innerHTML;
-    Array.from(document.querySelectorAll('.controls label'), (label) => {
-        const formX = label.children[0];
-        const formY = label.children[1];
-        const nameX = formX.name;
-        const nameY = formY.name;
-        const percentageX = payload
-            .split(nameX)[1]
-            .split(':')[1]
-            .split(';')[0];
-        const percentageY = payload
-            .split(nameY)[1]
-            .split(':')[1]
-            .split(';')[0];
-
-        const actualX = formX.min - formX.max;
-        console.log('actualX', actualX);
-
-        const {width, height} = label.getBoundingClientRect();
-        const top = (percentageX / 100) * (width);
-        const left = (percentageY / 100) * (height);
-        console.log(nameX, percentageX, width, 'top', top);
-        console.log(nameY, percentageX, height, 'left', left);
-        // label.scrollTo({
-        //     top,
-        //     left,
-        //     behavior: 'smooth'
-        // });
     });
 };
-
 // Shows QR code for a given panel
 const showPanelQR = (dialogQuerySelector, type) => {
     const dialogQuerySelectorCanvas = `${dialogQuerySelector} #qr-code-dialog-canvas`;
