8717f497497f1df46dd6ee3341d5f84aac0c6205
newmediapilot <marcin@newmediapilot.com>
2024-12-10 01:41:02 -0500

    Update dashboard_buttons.js

--- src/assets/script/dashboard_buttons.js
+++ src/assets/script/dashboard_buttons.js
@@ -1,52 +1,43 @@
-const dashboardButtons = (type, containerEl, toggleEl) => {
-  let payload = [];
-  const editLabel = toggleEl.getAttribute("data-edit");
-  const doneLabel = toggleEl.getAttribute("data-done");
-
-  const notifyEl = document.documentElement;
-  const states = [
-    `<span><em>✏</em>Edit</span>`,
-    `<span><em>✅</em>Done</span>`,
-  ];
-
-  toggleEl.innerHTML = states[0];
-
-  const sortable = new Sortable(containerEl, {
-    // swap: true, TODO: rewrite interpreter to use this!
-    animation: 333,
-    forceFallback: true,
-    removeCloneOnHide: true,
-    ghostClass: "pointer-events-none",
-    onEnd: (event) => {
-      payload.push(event.oldIndex);
-      payload.push(event.newIndex);
-    },
-  });
-
-  sortable.option("disabled", true);
-
-  toggleEl.addEventListener("click", () => {
-    sortable.option("disabled", !sortable.option("disabled"));
-
-    notifyEl.classList.toggle("edit-active");
-
-    const isDisabled = sortable.option("disabled");
-    toggleEl.innerHTML = isDisabled ? states[0] : states[1];
-
-    if (isDisabled) {
-      axios
-        .get("/public/config/update", {
-          params: {
-            type,
-            payload: JSON.stringify(payload),
-          },
-        })
-        .finally(() => {
-          socketEmitReload();
-          document.querySelector("#menu-toggle").checked = false;
-        });
-
-      payload = [];
-    }
-  });
+const dashboardButtons = () => {
+    let payload = [];
+    const type = "signals:order";
+    const containerEl = document.querySelector(".dashboard");
+    const toggleEl = document.querySelector("#dashboard--toggle");
+    const notifyEl = document.documentElement;
+    const states = [
+        `<span><em>✏</em>Edit</span>`,
+        `<span><em>✅</em>Done</span>`,
+    ];
+    toggleEl.innerHTML = states[0];
+    const sortable = new Sortable(containerEl, {
+        // swap: true, TODO: rewrite interpreter to use this!
+        animation: 333,
+        forceFallback: true,
+        removeCloneOnHide: true,
+        ghostClass: "pointer-events-none",
+        onEnd: (event) => {
+            payload.push(event.oldIndex);
+            payload.push(event.newIndex);
+        },
+    });
+    sortable.option("disabled", true);
+    toggleEl.addEventListener("click", () => {
+        sortable.option("disabled", !sortable.option("disabled"));
+        notifyEl.classList.toggle("edit-active");
+        toggleEl.innerHTML = sortable.option("disabled") ? states[0] : states[1];
+        if (sortable.option("disabled")) {
+            axios
+                .get("/public/config/update", {
+                    params: {
+                        type,
+                        payload: JSON.stringify(payload),
+                    },
+                })
+                .finally(() => {
+                    socketEmitReload();
+                    document.querySelector("#menu-toggle").checked = false;
+                });
+            payload = [];
+        }
+    });
 };
