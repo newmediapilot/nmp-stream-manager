34a01519663556719bc89c2912bbec5fda100e70
newmediapilot <marcin@newmediapilot.com>
2024-12-10 10:13:37 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const {startServices} = require("./modules/start");
 const {publicSignalCreate} = require("./modules/public/signal");
 const {publicConfigUpdate} = require("./modules/public/config");
 const {publicStyleUpdate} = require("./modules/public/style");
-const {publicbpmPing} = require("./modules/sensor/ping");
+const {publicBpmPing} = require("./modules/sensor/ping");
 const {twitchLogin, twitchLoginSuccess} = require("./modules/twitch/login");
 const app = express();
 // Just log
@@ -32,6 +32,6 @@ app.all(ROUTES.PUBLIC_SIGNAL_CREATE, publicSignalCreate);
 app.all(ROUTES.PUBLIC_CONFIG_UPDATE, publicConfigUpdate);
 app.all(ROUTES.PUBLIC_STYLE_UPDATE, publicStyleUpdate);
 // Pings
-app.all(ROUTES.PUBLIC_BPM_PING, publicbpmPing);
+app.all(ROUTES.PUBLIC_BPM_PING, publicBpmPing);
 // Init
 startServices(app);
