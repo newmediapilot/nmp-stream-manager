db9bf344e434763bdf39b0c516da6ea9c911af5e
newmediapilot <marcin@newmediapilot.com>
2024-12-10 04:24:49 -0500

    writing compiler

--- compiler/compile.js
+++ compiler/compile.js
@@ -1,38 +1,39 @@
 const fs = require("fs");
 const path = require("path");
-const { execSync } = require('child_process');
+const {exec, execSync} = require('child_process');
 const combinedJsonPath = path.join(process.cwd(), ".combined.json");
 const data = JSON.parse(fs.readFileSync(combinedJsonPath, "utf-8"));
-
+execSync("node ./compiler/combine.js");
 fs.rmSync("./.dist", {recursive: true, force: true});
-
 Object.keys(data)
-// Sort by length of path to ensure faster file creation
-    .sort((a, b) => a.split('\\').length - b.split('\\').length)
-    // Alphabetize to match system
-    .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
-    // Create directory scaffolding
     .map(path => {
         const writePath = path.split('\\').slice(0, -1).join('\\');
         const fullPath = `./.dist/${writePath}`;
-        // Ensure the parent directories exist, creating them if necessary
         if (!fs.existsSync(fullPath)) {
-            fs.mkdirSync(fullPath, {recursive: true});  // Create parent directories if they don't exist
+            fs.mkdirSync(fullPath, {recursive: true});
             console.log('Directory created successfully', fullPath);
         }
         return path;
     })
-    // Compile
+    .map(path => {
+        const content = data[path];
+        const fileName = path.split('\\').pop();
+        const isModule = path.includes('\\modules\\');
+        console.log('isModule', isModule, fileName);
+        return path;
+    })
     .map(path => {
         const fullPath = `./.dist/${path}`;
         const content = data[path];
         fs.writeFileSync(fullPath, content, {encoding: "utf-8"});
         return path;
     });
-
-fs.copyFileSync(".env", ".dist/.env");
-
-// Run the server
-execSync(`node ./.dist/${"src/index.js"}`)
-
+return;
+fs.copyFileSync(".env", "./.dist/.env");
 console.log(".dist/.env copied");
+// Start server
+console.log("starting services...");
+setTimeout(() => {
+    console.log('services started');
+}, 1000);
+execSync(`node ./.dist/${"src/index.js"}`);
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-node compiler/combine.js
+
 node compiler/compile.js
