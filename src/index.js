854380e2694098170f1451822be1019a750ec2c6
newmediapilot <marcin@newmediapilot.com>
2024-12-10 12:05:35 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(requestLogger);
 // Security headers
 app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "frame-ancestors 'self' https://localhost https://192.168.0.* https://*.twitch.tv;");
+    // res.setHeader('Content-Security-Policy', "frame-ancestors 'self' https://localhost https://192.168.0.* https://*.twitch.tv;");
     next();
 });
 // Twitch API Start
