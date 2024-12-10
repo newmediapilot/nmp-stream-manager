c59557971fe1a1be504aac38bece0d6abd11d412
newmediapilot <marcin@newmediapilot.com>
2024-12-10 10:42:01 -0500

    generation works

--- compiler/compile.js
+++ compiler/compile.js
@@ -4,13 +4,23 @@ const {execSync} = require('child_process');
 const request = require('sync-request');
 execSync("node ./compiler/combine.js");
 let jsonDataString = fs.readFileSync("./.combined.json", "utf-8");
+// First pass before bundling
+jsonDataString = jsonDataString.replace(/\?cb={{cache_buster}}/g, "");
 jsonDataString = jsonDataString.replace(/TWITCH_LOGIN/g, "WITCH_TUGGING");
-jsonDataString = jsonDataString.replace(/PUBLIC_INDEX/g, "PUBIC_SERVICE");
+jsonDataString = jsonDataString.replace(/PUBLIC_DASHBOARD/g, "CONTROL_PANEL_O_MATIC");
+jsonDataString = jsonDataString.replace(/PUBLIC_SETTINGS/g, "SETTING_SPREE");
+jsonDataString = jsonDataString.replace(/PUBLIC_MODULES/g, "MYSTERY_MODULES");
+jsonDataString = jsonDataString.replace(/PUBLIC_FEATURE_EMBED/g, "MAGIC_EMBEDDING");
+jsonDataString = jsonDataString.replace(/PUBLIC_BPM_EMBED/g, "BEAT_BOX_EMBED");
+jsonDataString = jsonDataString.replace(/PUBLIC_SIGNAL_CREATE/g, "SIGNAL_FIREWORKS");
+jsonDataString = jsonDataString.replace(/PUBLIC_CONFIG_UPDATE/g, "CONFIG_TWEAKS");
+jsonDataString = jsonDataString.replace(/PUBLIC_STYLE_UPDATE/g, "STYLE_SHUFFLE");
+jsonDataString = jsonDataString.replace(/PUBLIC_BPM_PING/g, "BEAT_THROB");
 // Clean
 fs.rmSync("./.dist", {recursive: true, force: true});
 const data = JSON.parse(jsonDataString);
-Object.keys(data)
 // Copy files and create directories
+Object.keys(data)
     .map(path => {
         const writePath = path.split('\\').slice(0, -1).join('\\');
         const fullPath = `./.dist/${writePath}`;
@@ -20,9 +30,14 @@ Object.keys(data)
         }
         return path;
     })
-    // Remove "cb={{cache_buster}}"
+    // Remove user written items
     .map(path => {
-        data[path] = data[path].replace(/\?cb={{cache_buster}}/g, "");
+        // data[path] = data[path].replace(/\/\*.*\*\//g, '');
+        // data[path] = data[path].replace(/\/\*.*\*\//g, '');
+        // data[path] = data[path].replace(/\/\*.*\*\//g, '');
+        // data[path] = data[path].replace(/<!--.*-->/g, '');
+        // data[path] = data[path].replace(/<!--.*-->/g, '');
+        // data[path] = data[path].replace(/<!--.*-->/g, '');
         return path;
     })
     // Inline cdn
@@ -30,31 +45,31 @@ Object.keys(data)
         data[path] = data[path].replace(/<script src="https:\/\/cdn.*>/g, (m) => {
             const src = m.match(/src="([^"]+)"/)[1];
             const res = request('GET', src);
-            return `<script data-script-compiled defer>${res.getBody('utf8')}</script>`;
+            return `<script data-path="${src}" defer>${res.getBody('utf8')}</script>`;
         });
         return path;
     })
     // Inline script
     .map(path => {
         data[path] = data[path].replace(/<script src="\/script.*>/g, (m) => {
+            const src = m.match(/src="([^"]+)"/)[1].split('/').join('');
             const dPath = Object.keys(data).find(key => {
-                const hrefKeys = m.match(/src="([^"]+)"/)[1].split('/').join('');
                 const pathKeys = key.split('\\').join('');
-                return pathKeys.endsWith(hrefKeys);
+                return pathKeys.endsWith(src);
             });
-            return `<script data-script-compiled script>${data[dPath]}</script>`
+            return `<script data-path="${src}" defer>${data[dPath]}</script>`
         });
         return path;
     })
     // Inline style
     .map(path => {
-        data[path] = data[path].replace(/<link src="\/style.*>/g, (m) => {
+        data[path] = data[path].replace(/<link href="\/style.*>/g, (m) => {
+            const hrefKeys = m.match(/href="([^"]+)"/)[1].split('/').join('');
             const dPath = Object.keys(data).find(key => {
-                const hrefKeys = m.match(/href="([^"]+)"/)[1].split('/').join('');
                 const pathKeys = key.split('\\').join('');
                 return pathKeys.endsWith(hrefKeys);
             });
-            return `<style data-style-compiled>${data[dPath]}</style>`
+            return `<style data-path="${hrefKeys}">${data[dPath]}</style>`
         });
         return path;
     })
