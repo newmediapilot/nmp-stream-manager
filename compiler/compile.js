b479e72027d1e019b641e1488a6f5c82713b5c74
newmediapilot <marcin@newmediapilot.com>
2024-12-10 03:56:00 -0500

    Update compile.js

--- compiler/compile.js
+++ compiler/compile.js
@@ -1,13 +1,17 @@
 const fs = require("fs");
 const path = require("path");
+const { execSync } = require('child_process');
 const combinedJsonPath = path.join(process.cwd(), ".combined.json");
 const data = JSON.parse(fs.readFileSync(combinedJsonPath, "utf-8"));
 
 fs.rmSync("./.dist", {recursive: true, force: true});
 
 Object.keys(data)
+// Sort by length of path to ensure faster file creation
     .sort((a, b) => a.split('\\').length - b.split('\\').length)
+    // Alphabetize to match system
     .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+    // Create directory scaffolding
     .map(path => {
         const writePath = path.split('\\').slice(0, -1).join('\\');
         const fullPath = `./.dist/${writePath}`;
@@ -18,11 +22,17 @@ Object.keys(data)
         }
         return path;
     })
+    // Compile
     .map(path => {
+        const fullPath = `./.dist/${path}`;
         const content = data[path];
-        // Implement logic for copying or handling the file content
+        fs.writeFileSync(fullPath, content, {encoding: "utf-8"});
         return path;
     });
 
 fs.copyFileSync(".env", ".dist/.env");
+
+// Run the server
+execSync(`node ./.dist/${"src/index.js"}`)
+
 console.log(".dist/.env copied");
