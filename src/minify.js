528877a53ecfc5729f4d2d5186a2fc0b5c220fd8
newmediapilot <marcin@newmediapilot.com>
2024-12-11 13:27:26 -0500

    Update minify.js

--- src/minify.js
+++ src/minify.js
@@ -1,15 +1,36 @@
 const {minify} = require('html-minifier-terser');
-const ROUTES = require("./routes");
+const { getParam } = require("./modules/store/manager");
 const requestCompressed = (req, res, next) => {
-    // const originalSend = res.send;
-    // res.send = (body) => {
-    //     if (req.path === ROUTES.PUBLIC_DASHBOARD) {
-    //         console.log('Request matches /desired-path', body);
-    //         originalSend.call(res, 'shit my balls');
-    //     }else{
-    //         originalSend.call(res, body);
-    //     }
-    // };
+    const originalSend = res.send;
+    res.send = (body) => {
+        if (body.startsWith("<!DOCTYPE html>")) {
+            const emoji_collection = getParam('emoji_collection');
+            emoji_collection.forEach((e,i) =>{
+                body = body.replace(RegExp(e, 'gm'), i);
+            });
+            // body = body.replace(/[\p{Emoji}\u200B-\u200D\uFE0F\u2B50]/gu, '');
+            // minify(body, {
+            //     collapseWhitespace: true,
+            //     removeComments: true,
+            //     removeRedundantAttributes: true,
+            //     useShortDoctype: true,
+            //     minifyJS: {
+            //         compress: true,
+            //         mangle: true,
+            //     },
+            //
+            //     minifyCSS: true,
+            //     output: {
+            //         ascii_only: false
+            //     }
+            // }).then((result) => {
+            //     console.log('result.minified', result);
+            // }).catch((e) => {
+            //     console.log('No minify available.', e);
+            // });
+        }
+        originalSend.call(res, body);
+    };
     next();
 };
 module.exports = {requestCompressed};
