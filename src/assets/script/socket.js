5a82ec5439d021132d2af11fa3b08d25e7549df3
newmediapilot <marcin@newmediapilot.com>
2024-12-11 01:41:01 -0500

    more refactoring

--- src/assets/script/socket.js
+++ src/assets/script/socket.js
@@ -1,5 +1,4 @@
 let $socketIO = null;// "private" variable
-
 const socketConnect = () => {
     if ($socketIO) return;
     $socketIO = io();
@@ -9,40 +8,30 @@ const socketConnect = () => {
     $socketIO.on("connect", () => console.log("Connected to the socket server"));
     console.log("Socket connection start");
 };
-
 const socketWatchStyle = () => {
-    socketConnect();
+
     console.log("socketWatchStyle");
     $socketIO.on("payload", (payload) => {
         payload.startsWith('style:set:') && callback(payload);
     });
 };
-
 const socketWatchFeatureSet = (callback) => {
-    socketConnect();
     console.log("socketWatchFeatureSet");
     $socketIO.on("payload", (payload) => {
         payload.startsWith('feature:set:') && callback(payload);
     });
 };
-
 const socketEmitStyle = (payload) => {
 // TODO: figure this out
 };
-
 const socketEmitReload = () => {
     axios.get("/public/signal/create?type=browser&description=reload");
 };
-
 // Socket will only emit reload for pages which are inactive
 const socketWatchReload = () => {
-    socketConnect();
-
     console.log("socketWatchReload");
-
     const timeout = 333;
     let to = null;
-
     const setFocusToken = () => {
         if (to) clearTimeout(to);
         document.documentElement.classList.add('focus');
@@ -50,16 +39,14 @@ const socketWatchReload = () => {
             document.documentElement.classList.remove('focus');
         }, timeout);
     };
-
     setFocusToken();
-
     document.documentElement.querySelectorAll('*').forEach((e) => e.addEventListener('touchstart', setFocusToken));
     document.documentElement.querySelectorAll('*').forEach((e) => e.addEventListener('touchend', setFocusToken));
     document.documentElement.querySelectorAll('*').forEach((e) => e.addEventListener('mousedown', setFocusToken));
     document.documentElement.querySelectorAll('*').forEach((e) => e.addEventListener('mouseup', setFocusToken));
-
     $socketIO.on("payload", (payload) => {
         if ("browser:reload" === payload) {
+            // reload inactive only
             const isFocusedActiveWindow = document.documentElement.classList.contains('focus');
             !isFocusedActiveWindow && window.location.reload();
         }
