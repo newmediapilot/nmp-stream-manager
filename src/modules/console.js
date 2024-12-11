0b991f6f2f461208e0001129db35a8859c5c539d
newmediapilot <marcin@newmediapilot.com>
2024-12-11 13:27:23 -0500

    Update console.js

--- src/modules/console.js
+++ src/modules/console.js
@@ -1,22 +1,13 @@
-/**
- * File: src\modules\console.js
- * Description: Logic and operations for src\modules\console.js.
- */
-
 const chalk = require("chalk");
-
 console.log2 = function (dir, ...args) {
     console.log(chalk.bgBlack.white(dir.slice(-12)), chalk.green(...args));
 };
-
 console.err2 = function (dir, ...args) {
     console.log(chalk.bgBlack.white(dir.slice(-12)), chalk.red(...args));
 };
-
 console.info2 = function (dir, ...args) {
     console.log(chalk.bgBlack.white(dir.slice(-12)), chalk.blue(...args));
 };
-
-console.warn2 = function (dir, ...args) {
+ole.warn2 = function (dir, ...args) {
     console.log(chalk.bgBlack.white(dir.slice(-12)), chalk.yellow(...args));
 };
