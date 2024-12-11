f88146315bf881b0d6e50332690411b7a7d66cd6
newmediapilot <marcin@newmediapilot.com>
2024-12-11 14:13:23 -0500

    Update minify.js

--- src/minify.js
+++ src/minify.js
@@ -8,26 +8,16 @@ const requestCompressed = (req, res, next) => {
             emoji_collection.forEach((e,i) =>{
                 body = body.replace(RegExp(e, 'gm'), i);
             });
-            // body = body.replace(/[\p{Emoji}\u200B-\u200D\uFE0F\u2B50]/gu, '');
-            // minify(body, {
-            //     collapseWhitespace: true,
-            //     removeComments: true,
-            //     removeRedundantAttributes: true,
-            //     useShortDoctype: true,
-            //     minifyJS: {
-            //         compress: true,
-            //         mangle: true,
-            //     },
-            //
-            //     minifyCSS: true,
-            //     output: {
-            //         ascii_only: false
-            //     }
-            // }).then((result) => {
-            //     console.log('result.minified', result);
-            // }).catch((e) => {
-            //     console.log('No minify available.', e);
-            // });
+            const indexOf = body.indexOf('<aside class=items">');
+            console.log(body.substr(indexOf-50, 50));
+            minify(body, {
+                collapseWhitespace: true,
+                removeComments: true,
+            }).then((result) => {
+                //console.log('result.minified', result);
+            }).catch((e) => {
+                //console.log('No minify available.', e);
+            });
         }
         originalSend.call(res, body);
     };
