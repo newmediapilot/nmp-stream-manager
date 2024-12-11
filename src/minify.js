ff6d9508ba90913fb503b2644850852257dc092b
newmediapilot <marcin@newmediapilot.com>
2024-12-11 10:43:52 -0500

    Create minify.js

---
+++ src/minify.js
@@ -0,0 +1,13 @@
+const {minify} = require('html-minifier-terser');
+const ROUTES = require("./routes");
+const requestCompressed = (req, res, next) => {
+    const originalSend = res.send;
+    res.send = (body) => {
+        if (req.path === ROUTES.PUBLIC_DASHBOARD) {
+            console.log('Request matches /desired-path', body);
+        }
+        originalSend.call(res, body);
+    };
+    next();
+}
+module.exports = {requestCompressed};
