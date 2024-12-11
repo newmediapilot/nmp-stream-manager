19ef8d8e03bd51f154f73a12475dd6bd17b4614a
newmediapilot <marcin@newmediapilot.com>
2024-12-11 12:05:51 -0500

    Update minify.js

--- src/minify.js
+++ src/minify.js
@@ -1,15 +1,15 @@
 const {minify} = require('html-minifier-terser');
 const ROUTES = require("./routes");
 const requestCompressed = (req, res, next) => {
-    const originalSend = res.send;
-    res.send = (body) => {
-        if (req.path === ROUTES.PUBLIC_DASHBOARD) {
-            console.log('Request matches /desired-path', body);
-            originalSend.call(res, 'shit my balls');
-        }else{
-            originalSend.call(res, body);
-        }
-    };
+    // const originalSend = res.send;
+    // res.send = (body) => {
+    //     if (req.path === ROUTES.PUBLIC_DASHBOARD) {
+    //         console.log('Request matches /desired-path', body);
+    //         originalSend.call(res, 'shit my balls');
+    //     }else{
+    //         originalSend.call(res, body);
+    //     }
+    // };
     next();
 };
 module.exports = {requestCompressed};
