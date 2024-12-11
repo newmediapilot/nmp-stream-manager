0e4fa173bbbc148b539dbb106d0e88b1d6c2e714
newmediapilot <marcin@newmediapilot.com>
2024-12-11 03:36:47 -0500

    more refactoring

--- src/assets/script/get_url.js
+++ src/assets/script/get_url.js
@@ -1,5 +1,4 @@
 const getURL = (path) => {
-    document.querySelectorAll('#toggle-nav-show').forEach(el => el.checked = false);// TODO: implement the real thing
     if (window.self !== window.top) {
         window.parent.postMessage(
             {type: 'path', message: path},
