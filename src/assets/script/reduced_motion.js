7c295443244090455d5132f5b73cbf79dfe07ede
newmediapilot <marcin@newmediapilot.com>
2024-12-11 13:47:51 -0500

    Create reduced_motion.js

---
+++ src/assets/script/reduced_motion.js
@@ -0,0 +1,14 @@
+const reducedMotion = ()=>{
+@media (prefers-reduced-motion: reduce) {
+    if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+        const reducedMotionEl = document.createElement('style');
+        reducedMotionEl.setAttribute('data-reduced-motion', 'data-reduced-motion');
+        document.head.insertBefore(reducedMotionEl, document.head.firstChild);
+    }
+    //     document.head.appendChild(document.createElement('<style data-reduced-motion>').innerHTML=`* {
+    //         transition: none !important;
+    //         animation: none !important;}`
+    //     }
+    // )
+    }
+};
