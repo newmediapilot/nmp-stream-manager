1a8bf460f4fb99000843eb18e84547126754c6f0
newmediapilot <marcin@newmediapilot.com>
2024-12-12 17:19:38 -0500

    Update tweet.js

--- src/modules/twitter/tweet.js
+++ src/modules/twitter/tweet.js
@@ -1,53 +1,33 @@
-/**
- * File: src\modules\twitter\tweet.js
- * Description: This file contains logic for managing src\modules\twitter\tweet operations.
- * Usage: Import relevant methods/functions as required.
- */
-
 const { TwitterApi } = require("twitter-api-v2");
 const { twitchMessageCreate } = require("../twitch/message");
 const HASHTAGS = "#aaknotstreams #live #twitch";
-
 let tweetResponses = [];
-
 const twitterClient = new TwitterApi({
   appKey: process.env.TWITTER_API_KEY,
   appSecret: process.env.TWITTER_API_SECRET,
   accessToken: process.env.TWITTER_ACCESS_TOKEN,
   accessSecret: process.env.TWITTER_ACCESS_SECRET,
 });
-
 async function twitterTweet(description) {
   try {
     if (!description) {
       console.err2(process.cwd(), "Missing description.");
       return false;
     }
-
     const me = await twitterClient.v2.me();
     const username = me.data.username;
-
     const text = `${description} ${HASHTAGS}`;
-
     console.log2(process.cwd(), "twitterTweet...", description);
-
     const tweetResponse = await twitterClient.v2.tweet({ text });
     const tweetURL = `https://twitter.com/${username}/status/${tweetResponse.data.id}`;
-
     console.log2(process.cwd(), "Tweet posted successfully:", tweetURL);
-
     await twitchMessageCreate("🤖 Tweet ready: " + tweetURL);
-
     // TODO: make it post a chat command too
-
     tweetResponses.push({ text });
-
     return tweetResponse.data.text;
   } catch (error) {
     console.err2(process.cwd(), `Error tweeting`, error);
-
     return false;
   }
 }
-
 module.exports = { twitterTweet };
