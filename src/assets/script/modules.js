d782dc045aa20923b81611eb845ded9da7ba14f0
newmediapilot <marcin@newmediapilot.com>
2024-12-13 01:32:04 -0500

    Update modules.js

--- src/assets/script/modules.js
+++ src/assets/script/modules.js
@@ -2,8 +2,8 @@ const applyModuleStyles = () => {
     Array.from(document.body.querySelectorAll('.controls label')).forEach((label) => {
         const {width} = label.getBoundingClientRect();
         console.log('width', width);
-        if (50 === width) label.scrollTo({top: 0, left: 0});
-        if (100 === width) label.scrollTo({top: 0, left: 0});
+        if (50 === width) label.scrollTo({top: 25, left: 50});
+        if (100 === width) label.scrollTo({top: 25, left: 25});
         console.log('applyModuleStyles');
     });
     // const payload = document.head.querySelector('#public_module_styles').innerHTML;
