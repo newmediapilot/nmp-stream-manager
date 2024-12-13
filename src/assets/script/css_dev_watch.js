488afa4e3b1b310f884ea6caa09bc9a4f10875fd
newmediapilot <marcin@newmediapilot.com>
2024-12-12 19:35:49 -0500

    Update css_dev_watch.js

--- src/assets/script/css_dev_watch.js
+++ src/assets/script/css_dev_watch.js
@@ -1,3 +1,7 @@
 const cssDevWatch = () => {
-
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutationRecord) {
+            console.log('style changed!');
+        });
+    });
 };
