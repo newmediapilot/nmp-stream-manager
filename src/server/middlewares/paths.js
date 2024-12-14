bf3ef4e6498c0d7dd67d3f534670c403fd3a0d98
newmediapilot <marcin@newmediapilot.com>
2024-12-14 10:10:41 -0500

    wip flatten

--- src/server/middlewares/paths.js
+++ src/server/middlewares/paths.js
@@ -1,11 +1,16 @@
 const paths = (req, res, next) => {
     const originalSend = res.send;
     res.send = (body) => {
-        if (body.startsWith("/public/")) {
-            // console.log
+        if (req.path.startsWith("/public/") && typeof body === 'string') {
+            body = body.replace(/\.\.\/client/g, (m)=>{
+
+                console.log('m', m);
+
+                return "";
+            });
         }
         originalSend.call(res, body);
     };
     next();
 };
-module.exports = {cache};
+module.exports = {paths};
--- src/server/nunjucks/config.js
+++ src/server/nunjucks/config.js
@@ -1,20 +1,18 @@
 const nunjucks = require("nunjucks");
 const express = require("express");
-const { getParam, getAllParams } = require("../store/manager");
-const { generateAssetLinks } = require("../nunjucks/preload.filter");
+const {getParam, getAllParams} = require("../store/manager");
 const configureNunjucks = (app) => {
-  const nunjucksEnv = nunjucks.configure("src/templates", {
-    autoescape: true,
-    express: app,
-    noCache: true,
-  });
-  nunjucksEnv.addFilter("getParam", getParam);
-  nunjucksEnv.addFilter("getAllParams", getAllParams);
-  nunjucksEnv.addFilter("urlencode", encodeURIComponent);
-  nunjucksEnv.addFilter("generateAssetLinks", generateAssetLinks);
-  nunjucksEnv.addFilter("getTime", () => new Date().getTime().toString());
-  app.set("view engine", "html");
-  app.use(express.static("src/client"));
-  appRef = app;
+    const nunjucksEnv = nunjucks.configure("src/templates", {
+        autoescape: true,
+        express: app,
+        noCache: true,
+    });
+    nunjucksEnv.addFilter("getParam", getParam);
+    nunjucksEnv.addFilter("getAllParams", getAllParams);
+    nunjucksEnv.addFilter("urlencode", encodeURIComponent);
+    nunjucksEnv.addFilter("cacheBuster", () => `?a=${new Date().getTime()}`);
+    app.set("view engine", "html");
+    app.use(express.static("src/client"));
+    appRef = app;
 };
-module.exports = { configureNunjucks };
+module.exports = {configureNunjucks};
