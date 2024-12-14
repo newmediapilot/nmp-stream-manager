d3692119ce9067c33cca69052666e096e4e357cc
newmediapilot <marcin@newmediapilot.com>
2024-12-14 04:49:26 -0500

    Update paths.js

--- src/server/middlewares/paths.js
+++ src/server/middlewares/paths.js
@@ -2,7 +2,7 @@ const paths = (req, res, next) => {
     const originalSend = res.send;
     res.send = (body) => {
         if (body.startsWith("/public/")) {
-
+            // console.log
         }
         originalSend.call(res, body);
     };
