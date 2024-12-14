6629b7f1bd907599ad8bab977705770ae24067b6
newmediapilot <marcin@newmediapilot.com>
2024-12-14 04:14:23 -0500

    Update logger.js

--- src/server/middlewares/logger.js
+++ src/server/middlewares/logger.js
@@ -1,19 +1,19 @@
 const ROUTES = require("../routes");
-requestLogger = (req, res, next) => {
-  // Ignore
-  if ([
-      ROUTES.API_BPM_PING
-  ].includes(req.path)) {
+const logger = (req, res, next) => {
+    // Ignore
+    if ([
+        ROUTES.API_BPM_PING
+    ].includes(req.path)) {
+        next();
+        return;
+    }
+    console.info2(
+        process.cwd(),
+        "REQUEST =>",
+        req.path,
+        "PARAMS =>",
+        JSON.stringify(req.query || {}, null, 4),
+    );
     next();
-    return;
-  }
-  console.info2(
-    process.cwd(),
-    "REQUEST =>",
-    req.path,
-    "PARAMS =>",
-    JSON.stringify(req.query || {}, null, 4),
-  );
-  next();
 };
-module.exports = { requestLogger };
+module.exports = {logger};
