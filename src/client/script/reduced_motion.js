efcc28475e5d4800827615fec2a5ad24498037f3
newmediapilot <marcin@newmediapilot.com>
2024-12-14 18:38:51 -0500

    rd started

--- src/client/script/reduced_motion.js
+++ src/client/script/reduced_motion.js
@@ -1,11 +1,9 @@
-// TODO: implement!
 const reducedMotion = () => {
+    document.documentElement.classList.add('reduced-motion');
     if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-        const reducedMotionEl = document.createElement('style');
-        reducedMotionEl.setAttribute('data-reduced-motion', 'data-reduced-motion');
-        document.head.insertBefore(reducedMotionEl, document.head.firstChild);
+        document.documentElement.classList.add('reduced-motion');
+        console.log('reducedMotion :: active');
+    } else {
+        console.log('reducedMotion :: off');
     }
-    const prmEl = document.createElement('<style data-reduced-motion>');
-    prmEl.innerHTML = "@media (prefers-reduced-motion: reduce) * { transition: none !important; animation: none !important; }";
-    document.head.appendChild(prmEl);
 };
