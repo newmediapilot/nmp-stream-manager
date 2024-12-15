8505d822d5156d1b435833e212476fd7b6464b89
newmediapilot <marcin@newmediapilot.com>
2024-12-15 15:25:38 -0500

    Update css_dev_watch.js

--- src/client/script/css_dev_watch.js
+++ src/client/script/css_dev_watch.js
@@ -1,21 +1,21 @@
 const cssDevWatch = () => {
-    // const targetNode = document.documentElement;
-    // const config = {
-    //     childList: true,
-    //     attributes: true,
-    //     subtree: true
-    // };
-    // const callback = (mutationsList) => {
-    //     for (const mutation of mutationsList) {
-    //         console.log(`Style changed on element: ${mutation.target.tagName}`);
-    //     }
-    // };
-    // const observer = new MutationObserver(callback);
-    // observer.observe(targetNode, config);
-    // Array.from(document.styleSheets)
-    //     .forEach(({href, cssRules}) => {
-    //         Array.from(cssRules).forEach(({cssText}) => {
-    //
-    //         });
-    //     });
+    const targetNode = document.documentElement;
+    const config = {
+        childList: true,
+        attributes: true,
+        subtree: true
+    };
+    const callback = (mutationsList) => {
+        for (const mutation of mutationsList) {
+            console.log(`Style changed on element: ${mutation.target.tagName}`);
+        }
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+    Array.from(document.styleSheets)
+        .forEach(({href, cssRules}) => {
+            Array.from(cssRules).forEach(({cssText}) => {
+
+            });
+        });
 };
