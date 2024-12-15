b825227ffe0fe40f40742fcf2996a024db1d40f2
newmediapilot <marcin@newmediapilot.com>
2024-12-14 21:51:36 -0500

    Update reduced_motion.js

--- src/client/script/reduced_motion.js
+++ src/client/script/reduced_motion.js
@@ -1,5 +1,4 @@
 const reducedMotion = () => {
-    document.documentElement.classList.add('reduced-motion');
     if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
         document.documentElement.classList.add('reduced-motion');
         console.log('reducedMotion :: active');
