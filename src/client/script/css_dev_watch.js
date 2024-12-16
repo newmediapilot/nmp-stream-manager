4719a51334013ccae904d2a978f8b9e8b9c5713e
newmediapilot <marcin@newmediapilot.com>
2024-12-15 20:18:52 -0500

    Update css_dev_watch.js

--- src/client/script/css_dev_watch.js
+++ src/client/script/css_dev_watch.js
@@ -1,18 +1,22 @@
 const cssDevWatch = () => {
     let memory = [];
     let changes = [];
-
-    Array.from(document.styleSheets)
-        .filter(sheet => sheet.href)
-        .sort((a, b) => a.href.length - b.href.length)
-        .sort((a, b) => a.href.localeCompare(b.href))
-        .forEach(sheet => Array.from(sheet.cssRules).forEach(
-            (cssRule) => {
-                memory.push([sheet.href, cssRule.cssText]);
-            })
-        );
-    cssDevWatch.save = () => {
+    let interval = -1;
+    cssDevWatch.setMemory = () => {
+        memory = [];
+        Array.from(document.styleSheets)
+            .filter(sheet => sheet.href)
+            .sort((a, b) => a.href.length - b.href.length)
+            .sort((a, b) => a.href.localeCompare(b.href))
+            .forEach(sheet => Array.from(sheet.cssRules).forEach(
+                (cssRule) => {
+                    memory.push([sheet.href, cssRule.cssText]);
+                })
+            );
+    };
+    cssDevWatch.createChanges = () => {
         let index = 0;
+        changes = [];
         Array.from(document.styleSheets)
             .filter(sheet => sheet.href)
             .sort((a, b) => a.href.length - b.href.length)
@@ -23,9 +27,32 @@ const cssDevWatch = () => {
                     const memCssHref = memCache[0];
                     const memCssText = memCache[1];
                     if (String(cssRule.cssText) !== String(memCssText)) {
-                        console.log('memCache', memCssHref, String(cssRule.cssText), String(memCssText));
+                        changes.push([memCssHref, String(cssRule.cssText)]);
                     }
                 })
             );
-    }
+    };
+    cssDevWatch.stop = () => {
+        clearInterval(interval);
+    };
+    cssDevWatch.start = () => {
+        cssDevWatch.setMemory();
+        interval = setInterval(() => cssDevWatch.check(), 1000);
+        console.log('cssDevWatch.start ::', interval);
+    };
+    cssDevWatch.check = () => {
+        if (changes.length) {
+            console.log('cssDevWatch.start ::changes detected', changes.length);
+            changes.forEach(([href, cssText], index) => {
+                changes.splice(changes[index].indexOf(changes[index]), 1);
+            });
+            console.log('cssDevWatch.start ::changes processed');
+            cssDevWatch.createChanges();
+        } else {
+            console.log('cssDevWatch.start ::memory', memory.length);
+            cssDevWatch.createChanges();
+            cssDevWatch.setMemory();
+        }
+    };
+    cssDevWatch.start();
 };
