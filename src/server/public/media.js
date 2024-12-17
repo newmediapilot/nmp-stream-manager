b4f2e9191d9056645d0b0e2af27101047da8f3db
newmediapilot <marcin@newmediapilot.com>
2024-12-17 02:51:31 -0500

    Update media.js

--- src/server/public/media.js
+++ src/server/public/media.js
@@ -1,18 +1,19 @@
 const fs = require("fs");
 const publicMediaUpdate = (req, res) => {
     try {
+        const {data, filename} = req.body;
+        if (!data || !filename) {
+            throw new Error("data missing");
+        }
+        console.log('data', filename);
         res.status(200).json({
             message: "Media updated successfully.",
         });
     } catch (error) {
-        console.log(
-            process.cwd(),
-            "Error processing media:",
-            error.message,
-        );
+        console.log("Error processing media:", error.message);
         res.status(500).json({error: error.message});
     } finally {
-        console.log(process.cwd(), "Media processing done.");
+        console.log("Media processing done.");
     }
 };
 module.exports = {
