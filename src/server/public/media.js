350c54ce26555673b439817766b13a6e3ad9ecdb
newmediapilot <marcin@newmediapilot.com>
2024-12-17 03:31:37 -0500

    Update media.js

--- src/server/public/media.js
+++ src/server/public/media.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const publicMediaUpdate = (req, res) => {
     try {
-        const {data, filename} = req.body;
-        if (!data || !filename) {
-            throw new Error("data missing");
+        const data = req.body.data;
+        const name = req.body.filename;
+        if (!data || !name) {
+            return res.status(400).json({error: 'Missing file data or filename'});
         }
-        console.log('data', filename);
+        fs.writeFileSync(`./src/client/.media/1.mp3`, Buffer.from(data, 'base64'));
         res.status(200).json({
             message: "Media updated successfully.",
         });
