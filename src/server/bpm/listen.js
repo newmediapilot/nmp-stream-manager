3e7459ce55876b1b8bbd07af59c943b878a9477c
newmediapilot <marcin@newmediapilot.com>
2024-12-17 21:44:40 -0500

    Update listen.js

--- src/server/bpm/listen.js
+++ src/server/bpm/listen.js
@@ -1,37 +1,46 @@
-const { setParam, getParam } = require("../store/manager");
-const { spawn } = require("child_process");
-async function createbpmRateServer(
-  exePath = ".bin/hds_desktop_windows.exe",
-) {
-  let MAX_REPORT = 5;
-  const child = spawn(exePath, [], { detached: false });
-  child.stdout.on("data", (data) => {
-    const log = data.toString();
-    const pref = "Received data: bpmRate:";
-    const bpmRate = parseInt(log.match(/\d+/)?.[0], 10);
+const {setParam, getParam} = require("../store/manager");
+const {spawn} = require("child_process");
 
-    log.startsWith(pref) &&
-      !isNaN(bpmRate) &&
-      setParam("sensor_bpm_rate", bpmRate, --MAX_REPORT > 0);
-  });
-  const shutdown = () => {
-    child.kill("SIGTERM");
-    process.exit();
-  };
-  process.on("exit", shutdown); // exit
-  process.on("SIGINT", shutdown); // Ctrl+C
-  process.on("SIGTERM", shutdown); // signals
-  process.on(
-    "uncaughtException",
-    (err) => console.log("Uncaught exception:", err) && shutdown(),
-  );
-  child.on("error", (err) =>
-    console.log(`Failed to start process: ${err.message}`),
-  );
-  child.on("close", (code) => console.log(`Process exited with code ${code}`));
+async function createBpmServer(
+    exePath = ".bin/hds_desktop_windows.exe",
+) {
+    app.listen(3476, () => {
+        console.log(
+            process.cwd(),
+            "Server running on 3476 at https://localhost",
+        );
+    });
+    let MAX_REPORT = 5;
+    const child = spawn(exePath, [], {detached: false});
+    child.stdout.on("data", (data) => {
+        const log = data.toString();
+        const pref = "Received data: bpmRate:";
+        const bpmRate = parseInt(log.match(/\d+/)?.[0], 10);
+
+        log.startsWith(pref) &&
+        !isNaN(bpmRate) &&
+        setParam("sensor_bpm_rate", bpmRate, --MAX_REPORT > 0);
+    });
+    const shutdown = () => {
+        child.kill("SIGTERM");
+        process.exit();
+    };
+    process.on("exit", shutdown); // exit
+    process.on("SIGINT", shutdown); // Ctrl+C
+    process.on("SIGTERM", shutdown); // signals
+    process.on(
+        "uncaughtException",
+        (err) => console.log("Uncaught exception:", err) && shutdown(),
+    );
+    child.on("error", (err) => {
+        console.log(`Failed to start process: ${err.message}`);
+        shutdown();
+    });
+    child.on("close", (code) => console.log(`Process exited with code ${code}`));
 }
+
 const getBpm = () => {
-  const bpmRate = getParam("sensor_bpm_rate");
-  return bpmRate ? `🤖 💜 ${bpmRate}` : `🤖 💜 Dunno.`;
+    const bpmRate = getParam("sensor_bpm_rate");
+    return bpmRate ? `🤖 💜 ${bpmRate}` : `🤖 💜 Dunno.`;
 };
-module.exports = { createbpmRateServer, getBpm };
+module.exports = {createBpmServer, getBpm};
