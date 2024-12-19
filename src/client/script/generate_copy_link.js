30c6be0ed17a89dbaf3872ed014926b27d29d333
newmediapilot <marcin@newmediapilot.com>
2024-12-18 23:56:19 -0500

    Update generate_copy_link.js

--- src/client/script/generate_copy_link.js
+++ src/client/script/generate_copy_link.js
@@ -1,19 +1,5 @@
-const generateCopyLink = (copyLinkButtonElement) => {
-    console.log('copyLinkButtonElement', copyLinkButtonElement);
-    // const copyLink = copyLinkButtonElement;
-    // copyLink.addEventListener('click', function (e) {
-    //     e.preventDefault(); // Prevent the default action of the link
-    //     const tempInput = document.createElement('input');
-    //     tempInput.value = copyLink.href; // Set its value to the link's href
-    //     document.body.appendChild(tempInput); // Append it to the body
-    //     // Select the text inside the input element
-    //     tempInput.select();
-    //     tempInput.setSelectionRange(0, 99999); // For mobile devices
-    //     // Copy the selected text to the clipboard
-    //     document.execCommand('copy');
-    //     // Remove the temporary input element
-    //     document.body.removeChild(tempInput);
-    //     // Optionally, show an alert or message to indicate the link was copied
-    //     alert('Link copied to clipboard!');
-    // });
+const generateCopyLink = (linkButtonElement) => {
+    linkButtonElement.addEventListener('click', () => {
+        navigator.clipboard.writeText(linkButtonElement.getAttribute('data-url'));
+    });
 };
