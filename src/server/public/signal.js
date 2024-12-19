f144312e39337ea9ca4d3665bfc1bc500c1b8bb9
newmediapilot <marcin@newmediapilot.com>
2024-12-18 20:38:28 -0500

    Update signal.js

--- src/server/public/signal.js
+++ src/server/public/signal.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const {execSync} = require("child_process");
 const {twitchMarkerCreate} = require("../twitch/marker");
 const {twitchMessageCreate} = require("../twitch/message");
 const {twitchAdCreate} = require("../twitch/ads");
