09bde4831f46b07caf30c01a62f80bd5937ab940
newmediapilot <marcin@newmediapilot.com>
2024-12-22 08:07:07 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,22 +16,14 @@ app.use(express.json({ limit: '200mb' }));
 app.use(logger);
 app.use(paths);
 app.all('*', (req, res, next) => {
-    const allowedOrigins = [
-        'https://dbdbdbdbdbgroup.com',
-        'https://192.168.8.22',
-    ];
     const origin = req.get('Origin');
-    if (allowedOrigins.includes(origin) || !origin) {
-        res.set('Access-Control-Allow-Origin', origin);
-        res.set('Access-Control-Allow-Credentials', 'true');
-        res.set('Access-Control-Allow-Methods', 'GET');
-        res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-        res.set('Access-Control-Allow-Private-Network', 'true');
-        if (req.method === 'OPTIONS') {
-            return res.status(204).end();
-        }
-    } else {
-        return res.status(403).send('Forbidden');
+    res.set('Access-Control-Allow-Origin', origin);
+    res.set('Access-Control-Allow-Credentials', 'true');
+    res.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    res.set('Access-Control-Allow-Private-Network', 'true');
+    if (req.method === 'OPTIONS') {
+        return res.status(204).end();
     }
     next();
 });
