c45a78dab891db05e63094d173f0ee45d5a76f34
newmediapilot <marcin@newmediapilot.com>
2024-12-22 04:01:52 -0500

    Update zombies.js

--- compiler/zombies.js
+++ compiler/zombies.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 fs.existsSync('.s3') && fs.rmdirSync('.s3', {recursive: true});
 fs.mkdirSync('.s3');
+fs.cpSync('./src/client/icon512_maskable.png', './.s3/client/icon512_maskable.png');
+fs.cpSync('./src/client/icon512_rounded.png', './.s3/client/icon512_rounded.png');
 const htmlString = fs.readFileSync('./src/templates/zombie.html', {encoding: 'utf-8'});
 const manifestJson = fs.readFileSync('./src/client/manifest.json', {encoding: 'utf-8'});
-[0, 1, 2].forEach((a, index) => {
-    const json = JSON.parse(String(manifestJson));
-    json.start_url = `${index}/index.html`;
-    fs.mkdirSync(`.s3/${index}`);
-    fs.writeFileSync(`.s3/${index}/index.html`, htmlString, {encoding: 'utf-8'});
-    fs.writeFileSync(`.s3/${index}/manifest.json`, json, {encoding: 'utf-8'});
-});
+[...Array.from({length: 255})].slice(1)
+    .forEach((a, index) => {
+        const json = JSON.parse(String(manifestJson));
+        json.start_url = `/${index}/welcome.html`;
+        fs.mkdirSync(`.s3/${index}`);
+        fs.writeFileSync(`.s3/${index}/welcome.html`, htmlString, {encoding: 'utf-8'});
+        fs.writeFileSync(`.s3/${index}/manifest.json`, JSON.stringify(json), {encoding: 'utf-8'});
+    });
