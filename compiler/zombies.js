564a5d7316c18db59c5d81f83b5fd9a5a0b3772f
newmediapilot <marcin@newmediapilot.com>
2024-12-22 05:02:09 -0500

    Update zombies.js

--- compiler/zombies.js
+++ compiler/zombies.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 fs.existsSync('.s3') && fs.rmdirSync('.s3', {recursive: true});
 fs.mkdirSync('.s3');
-fs.cpSync('./src/client/icon512_maskable.png', './.s3/client/icon512_maskable.png');
-fs.cpSync('./src/client/icon512_rounded.png', './.s3/client/icon512_rounded.png');
+fs.cpSync('./src/client/icon512_maskable.png', './.s3/icon512_maskable.png');
+fs.cpSync('./src/client/icon512_rounded.png', './.s3/icon512_rounded.png');
+fs.cpSync('./src/client/service-worker.js', './.s3/service-worker.js');
 const htmlString = fs.readFileSync('./src/templates/zombie.html', {encoding: 'utf-8'});
 const manifestJson = fs.readFileSync('./src/client/manifest.json', {encoding: 'utf-8'});
 [...Array.from({length: 255})].slice(1)
