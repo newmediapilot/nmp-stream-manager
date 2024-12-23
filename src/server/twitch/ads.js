893c0fc803e7435252c65fd1f3858204e3b44287
newmediapilot <marcin@newmediapilot.com>
2024-12-23 18:07:42 -0500

    Update ads.js

--- src/server/twitch/ads.js
+++ src/server/twitch/ads.js
@@ -1,36 +1,37 @@
-const { getSecret } = require("../store/manager");
+const {getSecret} = require("../store/manager");
+const fetch = require("node-fetch");
 const axios = require("axios");
-const { twitchMessageCreate } = require("./message");
+const {twitchMessageCreate} = require("./message");
 async function twitchAdCreate(length) {
-  try {
-    if (!length) length = 30;
-    const roundedLength = Math.ceil(Number(length) / 30) * 30;
-    console.log(
-      process.cwd(),
-      `Starting advertisement for ${roundedLength} seconds`,
-    );
-    const accessToken = getSecret("twitch_access_token");
-    const broadcasterId = getSecret("twitch_broadcaster_id");
-    await twitchMessageCreate(
-      `🤖 Get ready, we're running a ${roundedLength} second ad!`,
-    );
-    const response = await axios.post(
-      `https://api.twitch.tv/helix/channels/commercial`,
-      {
-        broadcaster_id: broadcasterId,
-        length: roundedLength,
-      },
-      {
-        headers: {
-          "Client-Id": process.env.TWITCH_CLIENT_ID,
-          Authorization: `Bearer ${accessToken}`,
-        },
-      },
-    );
-    return true;
-  } catch (error) {
-    console.log(process.cwd(), "Error starting advertisement:", error);
-    return false;
-  }
+    try {
+        if (!length || length>180) length = 30;
+        const roundedLength = Math.ceil(Number(length) / 30) * 30;
+        console.log(
+            process.cwd(),
+            `Starting advertisement for ${roundedLength} seconds`,
+        );
+        const accessToken = getSecret("twitch_access_token");
+        const broadcasterId = getSecret("twitch_broadcaster_id");
+        await fetch('https://api.twitch.tv/helix/channels/commercial', {
+            method: 'POST',
+            headers: {
+                'Client-Id': process.env.TWITCH_CLIENT_ID,
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                broadcaster_id: broadcasterId,
+                length: roundedLength
+            })
+        });
+        await twitchMessageCreate(
+            `🤖 Get ready, we're running a ${roundedLength} second ad!`,
+        );
+        return true;
+    } catch (error) {
+        console.log(process.cwd(), "Error starting advertisement:", error);
+        return false;
+    }
 }
-module.exports = { twitchAdCreate };
+
+module.exports = {twitchAdCreate};
