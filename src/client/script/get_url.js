39ddd83a262989ca6f4110d5fadcb7618f874c31
newmediapilot <marcin@newmediapilot.com>
2024-12-22 20:49:11 -0500

    Update get_url.js

--- src/client/script/get_url.js
+++ src/client/script/get_url.js
@@ -1,10 +1,9 @@
 const getURL = (path) => {
     if (window.self !== window.top) {
-        console.log('Sending message to parent');
-        // Correctly use postMessage to communicate with the parent
-        window.parent.postMessage(
+        console.log('Sending message to parent', window.parent);
+        window.top.postMessage(
             {type: 'path', message: path},
-            'https://192.168.0.22' // Ensure this matches the protocol and domain of the parent page
+            '*'
         );
     } else {
         console.log('getURL :: Not in an iframe');
