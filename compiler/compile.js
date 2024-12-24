a598b195034878a81ab48cd06e6b52888a069b70
newmediapilot <marcin@newmediapilot.com>
2024-12-23 22:43:54 -0500

    Update compile.js

--- compiler/compile.js
+++ compiler/compile.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
-const {sync:globSync} = require('glob');
-fs.existsSync('./.src') && fs.rmdirSync('./.src');
-globSync('./src/**/*.*').map(path => {
-    const dir = '';
-    console.log('path',path);
-});
-// fs.copyFileSync('./src', './.src');
+const {execSync} = require('child_process');
+const {sync: globSync} = require('glob');
+execSync('rm -rf ./.src');
+globSync('./src/**/*.*')
+    .map(path => path.replace('./src/', './.src/'))
+    .map(path => path.split('/').slice(0, -1).join('/'))
+    .sort((a, b) => a.length - b.length)
+    .map(dir => !fs.existsSync(dir) && fs.mkdirSync(dir));
+globSync('./src/**/*.*')
+    .map(path => !fs.existsSync(path) && fs.copyFileSync(path, path.replace('./src/', './.src/')));
