1189506922ed0df66f0f59e1e8b1d742b2e56c76
newmediapilot <marcin@newmediapilot.com>
2024-12-25 02:57:03 -0500

    Update socket.js

--- src/server/helper/socket.js
+++ src/server/helper/socket.js
@@ -2,7 +2,7 @@ const socketIo = require("socket.io");
 let io;
 const configureSocket = (server) => {
     if (!server) {
-        throw new Error("Server instance is required to configure Socket.IO.");
+        throw new Error("configureSocket :: server instance is required to configure Socket.IO.");
     }
     io = socketIo(server, {
         cors: {
@@ -13,31 +13,36 @@ const configureSocket = (server) => {
     });
     io.on("connection", (socket) => {
         console.log('socket', socket.handshake.address);
-        console.log(process.cwd(), "Client connected");
-        socket.on("message", (data) => {
-            console.log(process.cwd(), "Received message from client:", data);
+        console.log(process.cwd(), "configureSocket :: client connected");
+        socket.on("payload", (data) => {
+            console.log(process.cwd(), "configureSocket :: received payload from client:", data);
+            ("request:index" === data) && sentRequestResponse(data.split(":")[1]);
         });
         socket.on("disconnect", () => {
-            console.log(process.cwd(), "Client disconnected");
+            console.log(process.cwd(), "configureSocket :: client disconnected");
         });
     });
-    console.log(process.cwd(), "Socket.IO configured and attached to server.");
+    console.log(process.cwd(), "sendPayload :: socket.IO configured and attached to server.");
+};
+const sentRequestResponse = (type) => {
+    console.log("sentRequestResponse");
+    (type === "index") && socket.emit("payload", "response:index");
 };
 const sendPayload = (payload) => {
     if (!io) {
-        console.log("Socket.IO is not configured. Call configureSocket first.");
+        console.log("sendPayload :: socket.IO is not configured. Call configureSocket first.");
         return false;
     }
     if (!payload) {
-        console.log("Payload is required");
+        console.log("sendPayload :: payload is required");
         return false;
     }
     try {
         io.emit("payload", payload);
-        console.log("Sending payload:", payload);
+        console.log("sendPayload :: sending payload:", payload);
         return true;
     } catch (err) {
-        console.log("Error sending payload:", err);
+        console.log("sendPayload :: error sending payload:", err);
         return false;
     }
 };
