9cfe3d377e0eb3a3f11893e2918643a112e87e11
newmediapilot <marcin@newmediapilot.com>
2024-12-24 21:58:00 -0500

    Update get_url.js

--- src/client/script/get_url.js
+++ src/client/script/get_url.js
@@ -1,12 +1,17 @@
 const getURL = (path) => {
     if (window.self !== window.top) {
         console.log('Sending message to parent', window.parent);
+
+
+
         window.top.postMessage(
             {type: 'path', message: path},
             '*'
         );
+
+
+
     } else {
-        console.log('getURL :: Not in an iframe');
         window.location.href = path;
     }
 };
