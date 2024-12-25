856d608febc9256967446eecfa4a6f1df52a76eb
newmediapilot <marcin@newmediapilot.com>
2024-12-25 14:32:12 -0500

    Update get_url.js

--- src/client/script/get_url.js
+++ src/client/script/get_url.js
@@ -1,16 +1,7 @@
 const getURL = (path) => {
     if (window.self !== window.top) {
-        console.log('Sending message to parent', window.parent);
-
-
-
-        window.top.postMessage(
-            {type: 'path', message: path},
-            '*'
-        );
-
-
-
+        console.log('Sending message to $socketIO', path);
+        document.$socketIO.emit("path", path);
     } else {
         window.location.href = path;
     }
