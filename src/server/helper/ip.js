3b03b5e28474bce6b61e9bba378aab7c046c13ff
newmediapilot <marcin@newmediapilot.com>
2024-12-25 16:47:47 -0500

    Update ip.js

--- src/server/helper/ip.js
+++ src/server/helper/ip.js
@@ -1,12 +1,16 @@
 const os = require("os");
-function getIp() {
-  const interfaces = os.networkInterfaces();
-  for (const name in interfaces) {
-    for (const iface of interfaces[name]) {
-      if (iface.family === "IPv4" && !iface.internal) {
-        return iface.address;
-      }
+const getIp = () => {
+    const interfaces = os.networkInterfaces();
+    for (const name in interfaces) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === "IPv4" && !iface.internal) {
+                return iface.address;
+            }
+        }
     }
-  }
-}
-module.exports = { getIp };
+};
+const configureIp = () => {
+    setParam("device_ip", getIp());
+    setParam("device_ip_suffix", getIp().split('.').pop());
+};
+module.exports = {getIp, configureIp};
