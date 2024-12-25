b00f31b7da2c5a9fb64df636d8d5d7c55ff653ec
newmediapilot <marcin@newmediapilot.com>
2024-12-25 17:48:43 -0500

    Update start.js

--- src/server/start.js
+++ src/server/start.js
@@ -1,12 +1,12 @@
-const fs = require("fs");
 const https = require("https");
-const {setParam} = require("./store/manager");
+const {setParam, getSecret} = require("./store/manager");
 const {configureNunjucks} = require("./nunjucks/configure");
 const {createBpmServer} = require("./bpm/listen");
 const {initializePublicConfigs} = require("./public/config");
 const {initializePublicStyles} = require("./public/style");
 const {configureSocket} = require("./helper/socket");
 const ROUTES = require("./routes");
+
 async function startServices(app) {
     try {
         setParam("public_routes", ROUTES);
@@ -22,8 +22,8 @@ async function startServices(app) {
         await initializePublicStyles("style");
         configureNunjucks(app);
         const certs = {
-            key: fs.readFileSync("./localhost.key"),
-            cert: fs.readFileSync("./localhost.crt"),
+            key: getSecret('keys')['private'],
+            cert: getSecret('keys')['cert'],
         };
         const httpsServer = https.createServer(certs, app).listen(443, () => {
             console.log("startServices :: server running on 443 at https://localhost");
