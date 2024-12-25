028adc329ac877e1e08aa6d73f163fff64f19576
newmediapilot <marcin@newmediapilot.com>
2024-12-24 20:37:40 -0500

    Update zombies.js

--- compiler/zombies.js
+++ compiler/zombies.js
@@ -18,9 +18,9 @@ fs.copyFileSync('./src/client/icon512_maskable.png', './.s3/icon512_maskable.png
 fs.copyFileSync('./src/client/icon512_rounded.png', './.s3/icon512_rounded.png');
 [22].forEach((a, index) => {
     const json = JSON.parse(String(manifestJson));
-    json.start_url = `.s3/${index + 22}.html`;
-    fs.writeFileSync(`.s3/${index + 22}.json`, JSON.stringify(json), {encoding: 'utf-8'});
-    fs.writeFileSync(`.s3/${index + 22}.html`, 'placeholder...', {encoding: 'utf-8'});
+    json.start_url = `.s3/${22}.html`;
+    fs.writeFileSync(`.s3/${22}.json`, JSON.stringify(json), {encoding: 'utf-8'});
+    fs.writeFileSync(`.s3/${22}.html`, 'placeholder...', {encoding: 'utf-8'});
 });
 const paths = globSync(".s3/**/*.*")
     .map((filePath, index, arr) => {
