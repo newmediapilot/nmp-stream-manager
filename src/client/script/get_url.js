9f3032455d9bd834d825f028270ecc27acecb43c
newmediapilot <marcin@newmediapilot.com>
2024-12-25 20:32:41 -0500

    Update get_url.js

--- src/client/script/get_url.js
+++ src/client/script/get_url.js
@@ -1,7 +1,6 @@
 const getURL = (path) => {
     if (window.self !== window.top) {
-        console.log('Sending message to $socketIO', path);
-        document.$socketIO.emit("path", path);
+        socketEmitRoute();
     } else {
         window.location.href = path;
     }
