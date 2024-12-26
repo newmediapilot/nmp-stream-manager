0e1bccebaafb035538b34bfc4da8180a2e83e17c
newmediapilot <marcin@newmediapilot.com>
2024-12-25 20:46:28 -0500

    Update get_url.js

--- src/client/script/get_url.js
+++ src/client/script/get_url.js
@@ -1,6 +1,6 @@
 const getURL = (path) => {
     if (window.self !== window.top) {
-        socketEmitRoute();
+        axios.get(`${getPath("API_SIGNAL_CREATE")}?type=browser&description=route:${path}`);
     } else {
         window.location.href = path;
     }
