53df56d5c25bab6b47d21ecd3b878d3efd647ca8
newmediapilot <marcin@newmediapilot.com>
2024-12-28 11:27:48 -0500

    Update concat.js

--- compiler/concat.js
+++ compiler/concat.js
@@ -1,30 +1,26 @@
 const fs = require('fs');
 const {sync: globSync} = require('glob');
-
-const server = globSync('./src/**/*.*')
-    .sort((a, b) => a.localeCompare(b))
-    .filter(path => path.startsWith('./src/server/'))
-    .map(path => fs.readFileSync(path))
-    .join('\r\n');
-
-const lines = server.split('\r\n')
-    .sort((a, b) => {
-        if (a.startsWith("module.exports") && !b.startsWith("module.exports")) return -1;
-        if (!a.startsWith("module.exports") && b.startsWith("module.exports")) return 1;
-        return 0;
+const server = globSync('./src/server/**/*.js')
+    .map(path => {
+        console.log('concat :: path', path);
+        return path;
     })
-    .sort((a, b) => {
-        if (a.includes(`require("`)) return -1;
-        if (!a.includes(`require("`)) return 1;
-        return 0;
+    .map(path => {
+        const contents = fs.readFileSync(path, {encoding: "utf-8"});
+        return {
+            path,
+            contents,
+        }
     })
-    .sort((a, b) => {
-        if (a.includes(`require("./`)) return -1;
-        if (!a.includes(`require("./`)) return 1;
-        return 0;
-    });
+    .map(({path, contents}) => {
+        if (path.startsWith('./src/server')) {
+            return {contents: contents, path};
+        } else {
+            return {contents, path};
+        }
+    })
+    .map(({contents}) => contents)
+    .join('');
 
-fs.writeFileSync('./.server.js', lines.join('\r\n'), {encoding: 'utf-8'});
-
-console.log('lines', lines.slice(0,120));
-console.log('server', server.length);
+fs.writeFileSync('./.server.js', server, {encoding: 'utf-8'});
+console.log('concat :: path', server.substr(0, 400));
