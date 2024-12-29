bc7b190dd063f20fbc2cbb2bf583c3f257650105
newmediapilot <marcin@newmediapilot.com>
2024-12-29 18:13:52 -0500

    Update environment.js

--- src/server/nunjucks/environment.js
+++ src/server/nunjucks/environment.js
@@ -1,18 +1,23 @@
 const nunjucks = require("nunjucks");
 const express = require("express");
 const {getParam} = require("../store/manager");
-let nunjucksEnv = {};
-const configureNunjucks = (app) => {
-    nunjucksEnv = nunjucks.configure('src/templates', {
+const environment = (app = null) =>{
+    return nunjucks.configure('src/templates', {
         autoescape: true,
         express: app,
         noCache: true,
     });
-    nunjucksEnv.addFilter("getParamFilter", getParam);
+};
+const configureNunjucks = (app) => {
+    const env = environment(app);
+    env.addFilter("getParamFilter", getParam);
     app.set("view engine", "html");
     app.use(express.static("src/client"));
 };
 const renderStringTemplate = (res, content) => {
-    res.send(nunjucksEnv.renderString(content));
+    const env = environment();
+    console.log('getParam', getParam);
+    env.addFilter("getParamFilter", getParam);
+    res.send(env.renderString(content));
 };
 module.exports = {configureNunjucks, renderStringTemplate};
