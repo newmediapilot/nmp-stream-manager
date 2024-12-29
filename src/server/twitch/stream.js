9ea7634f38ae5b6f9d934a1ce270d9c41da870da
newmediapilot <marcin@newmediapilot.com>
2024-12-28 19:07:46 -0500

    Update stream.js

--- src/server/twitch/stream.js
+++ src/server/twitch/stream.js
@@ -1,33 +1,33 @@
 const tmi = require("tmi.js");
-const { getSecret } = require("../store/manager");
-const { parseCommand } = require("./commands");
-async function watchMessages() {
-  const token = getSecret("twitch_access_token");
-  const channel = getSecret("twitch_channel_id");
-  const client = new tmi.Client({
-    options: { debug: true },
-    connection: { reconnect: true, secure: true },
-    identity: {
-      username: channel,
-      password: ["oauth:", token].join(""),
-    },
-    channels: [channel],
-  });
-  client
-    .connect()
-    .then(() =>
-      console.log(
-        process.cwd(),
-        "Connected to Twitch chat for channel:",
-        channel,
-      ),
-    )
-    .catch((err) =>
-      console.log( "Error connecting to Twitch chat:", err),
+const {getSecret} = require("../store/manager");
+const {parseCommand} = require("./commands");
+const watchMessages = async () => {
+    const token = getSecret("twitch_access_token");
+    const channel = getSecret("twitch_channel_id");
+    const client = new tmi.Client({
+        options: {debug: true},
+        connection: {reconnect: true, secure: true},
+        identity: {
+            username: channel,
+            password: ["oauth:", token].join(""),
+        },
+        channels: [channel],
+    });
+    client
+        .connect()
+        .then(() =>
+            console.log(
+                process.cwd(),
+                "Connected to Twitch chat for channel:",
+                channel,
+            ),
+        )
+        .catch((err) =>
+            console.log("Error connecting to Twitch chat:", err),
+        );
+    client.on("message", parseCommand);
+    client.on("disconnected", (reason) =>
+        console.log("Disconnected from Twitch chat:", reason),
     );
-  client.on("message", parseCommand);
-  client.on("disconnected", (reason) =>
-    console.log( "Disconnected from Twitch chat:", reason),
-  );
-}
-module.exports = { watchMessages };
+};
+module.exports = {watchMessages};
