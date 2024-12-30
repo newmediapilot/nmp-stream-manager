9c98a45a9ef0d72acce59bc414997530d11b1c80
newmediapilot <marcin@newmediapilot.com>
2024-12-30 00:07:36 -0500

    Create snapshot.js

---
+++ compiler/snapshot.js
@@ -0,0 +1,15 @@
+// TODO: snapshot the pages and include them in the bucket, but have them talk to their local style/settings instances by interval
+// TODO: since pages will fetch the local values on reload the same system works
+// TODO: when the settings change we will upload them to S3
+// TODO: potentially turn off socketReload(), and make it load when an event has finished
+// TODO: pretty sure pages dont need reload anymore!
+// C:\Users\marcinzajkowski\Desktop\desktop\aaknot\server\src\templates\embed.html
+// C:\Users\marcinzajkowski\Desktop\desktop\aaknot\server\src\templates\embed-draw.html
+// C:\Users\marcinzajkowski\Desktop\desktop\aaknot\server\src\templates\embed-feature.html
+// C:\Users\marcinzajkowski\Desktop\desktop\aaknot\server\src\templates\embed-media.html
+// C:\Users\marcinzajkowski\Desktop\desktop\aaknot\server\src\templates\embed-sound.html
+// C:\Users\marcinzajkowski\Desktop\desktop\aaknot\server\src\templates\index.html
+// C:\Users\marcinzajkowski\Desktop\desktop\aaknot\server\src\templates\panel-actions.html
+// C:\Users\marcinzajkowski\Desktop\desktop\aaknot\server\src\templates\panel-dashboard.html
+// C:\Users\marcinzajkowski\Desktop\desktop\aaknot\server\src\templates\panel-draw.html
+// C:\Users\marcinzajkowski\Desktop\desktop\aaknot\server\src\templates\panel-layout.html
