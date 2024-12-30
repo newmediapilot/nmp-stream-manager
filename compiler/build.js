b2bf5e5ee0f715eb352ed1d41a80bab50e73c6c5
newmediapilot <marcin@newmediapilot.com>
2024-12-29 23:49:02 -0500

    add hashing

--- compiler/build.js
+++ compiler/build.js
@@ -1,12 +1,17 @@
 console.time("Build :: complete");
 const {execSync} = require("child_process");
-console.log("Build :: concat");
-execSync("node compiler/concat.js", {stdio: 'inherit'});
-console.log("Build :: compile");
-execSync("node compiler/compile.js", {stdio: 'inherit'});
-console.log("Build :: s3");
-execSync("node compiler/s3.js", {stdio: 'inherit'});
-console.log("Build :: clean");
-execSync("rm -rf .compiled", {stdio: 'inherit'});
-execSync("rm -rf .server.js", {stdio: 'inherit'});
+const crypto = require("crypto");
+let length = 1;
+Array.from({length})
+    .map((_, index) => crypto.createHash('sha256').update(String(index)).digest('hex'))
+    .forEach(hash => {
+            console.log("Build :: concat ---------------=> ", hash);
+            execSync(`node compiler/concat.js ${hash}`, {stdio: 'inherit'});
+            console.log("Build :: compile --------------=> ", hash);
+            execSync(`node compiler/compile.js ${hash}`, {stdio: 'inherit'});
+            console.log("Build :: s3 -------------------=> ", hash);
+            execSync(`node compiler/s3.js ${hash}`, {stdio: 'inherit'});
+            console.log("Build :: deployed ---=> ", hash);
+        }
+    );
 console.timeEnd("Build :: complete");
