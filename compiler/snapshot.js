4f0750fc97bfe7d1d0de3b66e80d77cc2791a495
newmediapilot <marcin@newmediapilot.com>
2024-12-30 18:19:03 -0500

    Update snapshot.js

--- compiler/snapshot.js
+++ compiler/snapshot.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
+const {execSync} = require('child_process');
 const {exec} = require('child_process');
 const {sync: globSync} = require('glob');
-const {execSync} = require('child_process');
-const server = exec('npm run serve', {stdio: 'inherit'});
+const server = exec('npm run serve');
 const hash = process.argv[2] || 'demo';
 globSync('.compiled/src/templates/*.html')
     .map(path => {
-        return {path, contents: execSync(`curl -k https://localhost/${hash}/${path.split('/').pop()}`, {stdio: 'pipe'}).toString()}
-    })
-    .map(({path, contents}) => {
-        fs.writeFileSync(`.tplt-${path.split('/').pop()}`, contents, {encoding: 'utf-8'});
+        const data = execSync(`curl -k https://localhost/${hash}/${path.split('/').pop()}`, {stdio: 'pipe'});
+        fs.writeFileSync(`.tplt-${path.split('/').pop()}`, data.toString(), {encoding: 'utf-8'});
     });
-process.kill(server.pid);
+process.exit(server.pid);
 process.exit(0);
