8e85b7e0a365ac80160c0256cf886007fbc6b7c1
newmediapilot <marcin@newmediapilot.com>
2024-12-30 15:05:19 -0500

    Update snapshot.js

--- compiler/snapshot.js
+++ compiler/snapshot.js
@@ -1,32 +1,15 @@
 const fs = require('fs');
 const {exec} = require('child_process');
+const {sync: globSync} = require('glob');
 const {execSync} = require('child_process');
 const server = exec('npm run serve', {stdio: 'inherit'});
 const hash = process.argv[2] || 'demo';
-[
-    ['.tplt-index.html', execSync('curl -k https://localhost/demo/index.html', {stdio: 'pipe'})],
-    ['.tplt-panel-dashboard.html', execSync('curl -k https://localhost/demo/panel-dashboard.html', {stdio: 'pipe'})],
-    ['.tplt-panel-actions.html', execSync('curl -k https://localhost/demo/panel-actions.html', {stdio: 'pipe'})],
-    ['.tplt-panel-layout.html', execSync('curl -k https://localhost/demo/panel-layout.html', {stdio: 'pipe'})],
-    ['.tplt-panel-draw.html', execSync('curl -k https://localhost/demo/panel-draw.html', {stdio: 'pipe'})],
-    ['.tplt-embed.html', execSync('curl -k https://localhost/demo/embed.html', {stdio: 'pipe'})],
-    ['.tplt-embed-feature.html', execSync('curl -k https://localhost/demo/embed-feature.html', {stdio: 'pipe'})],
-    ['.tplt-embed-media.html', execSync('curl -k https://localhost/demo/embed-media.html', {stdio: 'pipe'})],
-    ['.tplt-embed-draw.html', execSync('curl -k https://localhost/demo/embed-draw.html', {stdio: 'pipe'})],
-    ['.tplt-embed-sound.html', execSync('curl -k https://localhost/demo/embed-sound.html', {stdio: 'pipe'})],
-]
-    .map(([a, b]) => [a, b.toString()])
-    .forEach((payload, i, array) => {
-            let [a, b] = payload;
-            console.log('snapshot ::', a, i, '/', array.length - 1);
-            b = b.split('\r\n')
-                .map(line => line.replace('/demo/', `/${hash}/`))
-                .join('\r\n');
-            fs.writeFileSync(a, b);
-            if (i >= array.length - 1) {
-                process.kill(server.pid);
-                process.exit(0);
-            }
-        }
-    );
-
+globSync('.compiled/src/templates/*.html')
+    .map(path => {
+        return {path, contents: execSync(`curl -k https://localhost/${hash}/${path.split('/').pop()}`, {stdio: 'pipe'}).toString()}
+    })
+    .map(({path, contents}) => {
+        fs.writeFileSync(`.tplt-${path.split('/').pop()}`, contents, {encoding: 'utf-8'});
+    });
+process.kill(server.pid);
+process.exit(0);
