f68570ce74b55fe53ff1b585071ba7e4d8eb22fc
newmediapilot <marcin@newmediapilot.com>
2024-12-30 03:00:57 -0500

    Update snapshot.js

--- compiler/snapshot.js
+++ compiler/snapshot.js
@@ -3,21 +3,21 @@ const {exec} = require('child_process');
 const {execSync} = require('child_process');
 const server = exec('npm run serve', {stdio: 'inherit'});
 [
-    ['.compiled/src/templates/panel-dashboard.html', execSync('curl -k https://localhost/public/dashboard', {stdio: 'pipe'})],
-    ['.compiled/src/templates/panel-actions.html', execSync('curl -k https://localhost/public/actions', {stdio: 'pipe'})],
-    ['.compiled/src/templates/panel-layout.html', execSync('curl -k https://localhost/public/layout', {stdio: 'pipe'})],
-    ['.compiled/src/templates/panel-draw.html', execSync('curl -k https://localhost/public/draw', {stdio: 'pipe'})],
-    ['.compiled/src/templates/embed.html', execSync('curl -k https://localhost/public/embed', {stdio: 'pipe'})],
-    ['.compiled/src/templates/embed-feature.html', execSync('curl -k https://localhost/public/feature/embed', {stdio: 'pipe'})],
-    ['.compiled/src/templates/embed-media.html', execSync('curl -k https://localhost/public/media/embed', {stdio: 'pipe'})],
-    ['.compiled/src/templates/embed-draw.html', execSync('curl -k https://localhost/public/draw/embed', {stdio: 'pipe'})],
-    ['.compiled/src/templates/embed-sound.html', execSync('curl -k https://localhost/public/sound/embed', {stdio: 'pipe'})],
+    ['.tplt-panel-dashboard.html', execSync('curl -k https://localhost/public/dashboard', {stdio: 'pipe'})],
+    ['.tplt-panel-actions.html', execSync('curl -k https://localhost/public/actions', {stdio: 'pipe'})],
+    ['.tplt-panel-layout.html', execSync('curl -k https://localhost/public/layout', {stdio: 'pipe'})],
+    ['.tplt-panel-draw.html', execSync('curl -k https://localhost/public/draw', {stdio: 'pipe'})],
+    ['.tplt-embed.html', execSync('curl -k https://localhost/public/embed', {stdio: 'pipe'})],
+    ['.tplt-embed-feature.html', execSync('curl -k https://localhost/public/feature/embed', {stdio: 'pipe'})],
+    ['.tplt-embed-media.html', execSync('curl -k https://localhost/public/media/embed', {stdio: 'pipe'})],
+    ['.tplt-embed-draw.html', execSync('curl -k https://localhost/public/draw/embed', {stdio: 'pipe'})],
+    ['.tplt-embed-sound.html', execSync('curl -k https://localhost/public/sound/embed', {stdio: 'pipe'})],
 ]
     .map(([a, b]) => [a, b.toString()])
     .forEach((payload, i, array) => {
             const [a, b] = payload;
+            console.log('snapshot ::', a, i);
             fs.writeFileSync(a, b);
-            console.log('snapshot ::', a);
             if (i >= array.length - 1) {
                 console.log('snapshot :: end');
                 process.kill(server.pid);
