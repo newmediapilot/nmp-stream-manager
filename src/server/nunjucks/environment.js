8707eabc1a5ceb67ff13e09ae17ed5feba7f33e5
newmediapilot <marcin@newmediapilot.com>
2024-12-29 19:16:56 -0500

    Update environment.js

--- src/server/nunjucks/environment.js
+++ src/server/nunjucks/environment.js
@@ -2,14 +2,16 @@ const nunjucks = require("nunjucks");
 const express = require("express");
 const {getParam} = require("../store/manager");
 let env = null;
-const context = () => {
-    return {
+const generateContext = () => {
+    const c = {
         public_routes: getParam('public_routes'),
         device_ip: getParam('device_ip'),
         device_ip_suffix: getParam('device_ip_suffix'),
         dashboard_signals_config: getParam('dashboard_signals_config'),
         public_module_styles: getParam('public_module_styles'),
-    }
+    };
+    console.log('generateContext ::', c);
+    return c;
 };
 const configureNunjucks = (app) => {
     env = nunjucks.configure('src/templates', {
@@ -21,6 +23,8 @@ const configureNunjucks = (app) => {
     app.use(express.static("src/client"));
 };
 const renderStringTemplate = (res, content) => {
-    res.send(env.renderString(content, context()));
+    const ctx = generateContext();
+    console.log('renderStringTemplate.ctx', ctx);
+    res.send(env.renderString(content, ctx));
 };
 module.exports = {configureNunjucks, renderStringTemplate};
