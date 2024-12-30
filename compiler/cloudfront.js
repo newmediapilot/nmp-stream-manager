644f3f54018b2a4e53888b0d28d0f80d74d3a2be
newmediapilot <marcin@newmediapilot.com>
2024-12-30 03:40:34 -0500

    Create cloudfront.js

---
+++ compiler/cloudfront.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const AWS = require('aws-sdk');
+const cloudFront = new AWS.CloudFront();
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
+cloudFront.createInvalidation({
+    DistributionId: "E3IXY2ACIKURY1",
+    InvalidationBatch: {
+        CallerReference: `invalidate-${Date.now()}`,
+        Paths: {
+            Quantity: 1,
+            Items: ['/*'],
+        },
+    },
+}).promise().then(result => {
+    console.log('cloudfront :: invalidation started');
+}).catch(err => {
+    console.log('cloudfront :: invalidation error', err);
+});
