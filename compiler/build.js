9fdcab24b6c1a01185114a25de18ea4af093a491
newmediapilot <marcin@newmediapilot.com>
2024-12-30 22:50:00 -0500

    Update build.js

--- compiler/build.js
+++ compiler/build.js
@@ -1,8 +1,21 @@
 console.time("Build :: complete");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const {execSync} = require("child_process");
+const fs = require("fs");
+const {sync: globSync} = require("glob");
 const crypto = require("crypto");
 let length = 1;
+[
+    ...globSync('.*.html'),
+    ...globSync('.package'),
+    ...globSync('.compiled'),
+    ...globSync('.launch'),
+    ...globSync('.server'),
+    ...globSync('signals'),
+    ...globSync('style'),
+    ...globSync('package-lock.json'),
+].map(p => execSync(`rm -rf ${p}`));
+process.exit(0);
 Array.from({length})
     .map((_, index) => crypto.createHash('sha256').update(String(index)).digest('hex'))
     .forEach(hash => {
@@ -11,6 +24,10 @@ Array.from({length})
             execSync(`npm run compile ${hash}`, {stdio: 'inherit'});
             execSync(`npm run s3 ${hash}`, {stdio: 'inherit'});
             execSync(`npm run cloudfront ${hash}`, {stdio: 'inherit'});
+            fs.existsSync('.package') && execSync('rm -rf .package');
+            fs.existsSync('.compiled') && execSync('rm -rf .compiled');
+            fs.existsSync('.launch') && execSync('rm -rf .launch');
+            fs.existsSync('.server') && execSync('rm -rf .server');
         }
     );
 console.timeEnd("Build :: complete");
