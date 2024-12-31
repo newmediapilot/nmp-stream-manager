88160455097b78c9ca00a6386f848704234d138b
newmediapilot <marcin@newmediapilot.com>
2024-12-30 20:13:52 -0500

    Update snapshot.js

--- compiler/snapshot.js
+++ compiler/snapshot.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
-const {execSync} = require('child_process');
-const {exec} = require('child_process');
+const {exec, execSync} = require('child_process');
 const {sync: globSync} = require('glob');
-const server = exec('npm run serve');
-const hash = process.argv[2] || 'demo';
-globSync('.compiled/src/templates/*.html')
-    .map(path => {
-        const data = execSync(`curl -k https://localhost/${hash}/${path.split('/').pop()}`, {stdio: 'pipe'});
-        fs.writeFileSync(`.tplt-${path.split('/').pop()}`, data.toString(), {encoding: 'utf-8'});
-    });
-process.exit(server.pid);
-process.exit(0);
+const hash = process.argv.pop() || 'demo';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+let server = exec('npm run serve', {stdio: 'inherit'});
+try {
+    globSync('./src/templates/**/*.html')
+        .forEach((path, index, {length}) => {
+            console.log('snapshot :: path', path, `${index + 1}/${length}`);
+            const url = `https://localhost/${hash}/${path.split('/').pop()}`;
+            const contents = execSync(`curl -k ${url}`, {stdio: 'pipe'}).toString();
+            const outputPath = `.tplt-${path.split('/').pop()}`;
+            fs.writeFileSync(outputPath, contents, {encoding: 'utf-8'});
+            console.log(`snapshot :: write`, outputPath, `${index + 1}/${length}`);
+        });
+    console.log('snapshot :: done');
+} catch (error) {
+    console.error('snapshot :: error', error.message);
+}
