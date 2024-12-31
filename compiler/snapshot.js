b6e38c8f31bcae1896c716b79c51e5481426d30e
newmediapilot <marcin@newmediapilot.com>
2024-12-30 20:49:11 -0500

    Update snapshot.js

--- compiler/snapshot.js
+++ compiler/snapshot.js
@@ -1,21 +1,27 @@
-// const fs = require('fs');
-// const {exec, execSync} = require('child_process');
-// const {sync: globSync} = require('glob');
-// const hash = process.argv.pop() || 'demo';
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-// let server = exec('npm run serve', {stdio: 'inherit'});
-// try {
-//     globSync('./src/templates/**/*.html')
-//         .forEach((path, index, {length}) => {
-//             console.log('snapshot :: path', path, `${index + 1}/${length}`);
-//             const url = `https://localhost/${hash}/${path.split('/').pop()}`;
-//             const contents = execSync(`curl -k ${url}`, {stdio: 'pipe'}).toString();
-//             const outputPath = `.tplt-${path.split('/').pop()}`;
-//             fs.writeFileSync(outputPath, contents, {encoding: 'utf-8'});
-//             console.log(`snapshot :: write`, outputPath, `${index + 1}/${length}`);
-//         });
-//     console.log('snapshot :: done');
-// } catch (error) {
-//     console.error('snapshot :: error', error.message);
-// }
-
+const fs = require('fs');
+const {exec, execSync} = require('child_process');
+const {sync: globSync} = require('glob');
+const {chromium} = require('playwright');
+const hash = process.argv[2] || 'demo';
+console.log('snapshot :: hash', hash);
+exec('npm run serve', {stdio: 'inherit'});
+execSync('sleep 5');
+(async () => {
+    const browser = await chromium.launch();
+    const context = await browser.newContext({
+        ignoreHTTPSErrors: true
+    });
+    const page = await context.newPage();
+    globSync('./src/templates/**/*.html')
+        .map(async (path, index, {length}) => {
+            const url = `https://192.168.0.22/${hash}/${path.split('/').pop()}`;
+            console.log('snapshot :: path', path, url, `${index + 1}/${length}`);
+            await page.goto(url, {waitUntil: 'load'});
+            fs.writeFileSync(
+                `.tplt-${path.split('/').pop()}`,
+                await page.content(),
+                {encoding: 'utf-8'}
+            );
+        });
+    await browser.close(); // Close the browser
+})();
