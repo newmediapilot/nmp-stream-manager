83306436cf0b4c761c8bbcb4a2089a164e254305
newmediapilot <marcin@newmediapilot.com>
2024-12-31 21:03:29 -0500

    Update cert.js

--- src/cert.js
+++ src/cert.js
@@ -1,7 +1,36 @@
-try {
-    (async () => {
-        
-    })();
-} catch (error) {
-    console.error("cert :: error", error);
-}
+require("dotenv").config();
+const AWS = require('aws-sdk');
+(async () => {
+    try {
+        AWS.config.update({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            region: process.env.AWS_REGION,
+        });
+        const route53 = new AWS.Route53({ region: process.env.AWS_REGION });
+        const params = {
+            HostedZoneId: process.env.AWS_HOSTED_ZONE_ID,
+            ChangeBatch: {
+                Changes: [
+                    {
+                        Action: 'UPSERT',
+                        ResourceRecordSet: {
+                            Name: 'dbdbdbdbdbgroup.com.',
+                            Type: 'TXT',
+                            TTL: 300,
+                            ResourceRecords: [
+                                {
+                                    Value: '"zip zap zippidy zaw"'
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        };
+        const response = await route53.changeResourceRecordSets(params).promise();
+        console.log('TXT record added successfully:', response);
+    } catch (error) {
+        console.error('Error adding TXT record:', error);
+    }
+})();
