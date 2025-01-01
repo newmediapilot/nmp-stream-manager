fbd02c4b730c6c20c6c8491417ae0ca76a832b77
newmediapilot <marcin@newmediapilot.com>
2025-01-01 18:28:26 -0500

    Delete cert.js

--- src/proxy/cert.js
+++
@@ -1,7 +0,0 @@
-const password123 = `localPa$$123`;
-const pathIn = `C:\\Users\\marcinzajkowski\\Desktop\\desktop\\aaknot\\server\\.cert\\cert.pfx`;
-const pathOut = `C:\\Users\\marcinzajkowski\\Desktop\\desktop\\aaknot\\server\\.cert\\cert.key`;
-const fs = require('fs');
-const path = require('path');
-const {execSync} = require('child_process');
-execSync(`openssl pkcs12 -in "${pathIn}" -nocerts -out "${pathOut}"`);
