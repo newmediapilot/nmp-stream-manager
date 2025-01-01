638864f2486713f468d42d511a05cb2bf309e56a
newmediapilot <marcin@newmediapilot.com>
2025-01-01 17:47:05 -0500

    Update cert.js

--- src/proxy/cert.js
+++ src/proxy/cert.js
@@ -1,14 +1,7 @@
+const password123 = `localPa$$123`;
+const pathIn = `C:\\Users\\marcinzajkowski\\Desktop\\desktop\\aaknot\\server\\.cert\\cert.pfx`;
+const pathOut = `C:\\Users\\marcinzajkowski\\Desktop\\desktop\\aaknot\\server\\.cert\\cert.key`;
 const fs = require('fs');
 const path = require('path');
-const {sync: globSync} = require('glob');
-const windowsPath = 'C:\\ProgramData\\certify\\assets\\api.dbdbdbdbdbgroup.com';
-const normalizedPath = path.normalize(windowsPath);
-const pfx = globSync(`${normalizedPath}/*.pfx`)
-    .map(filePath => ({
-        path: filePath,
-        mtime: fs.statSync(filePath).mtime
-    }))
-    .sort((a, b) => a.mtime - b.mtime)
-    .map(({path}) => path).pop();
-////
-console.log('pfx', pfx);
+const {execSync} = require('child_process');
+execSync(`openssl pkcs12 -in "${pathIn}" -nocerts -out "${pathOut}"`);
