6ff4bf98fe3beb971dfa15c7a2270e5f718a7899
newmediapilot <marcin@newmediapilot.com>
2025-01-01 19:10:26 -0500

    Update ec2.js

--- src/proxy/ec2.js
+++ src/proxy/ec2.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const startup = fs.readFileSync('./src/proxy/startup.sh', {encoding: 'utf-8'});
 const lines = startup.split('\r\n')
-    .map(line => line.replace('cert-xxx', `"${fs.readFileSync('.cert/cert.crt', {encoding: 'utf-8'})}"`))
-    .map(line => line.replace('key-xxx', `"${fs.readFileSync('.cert/cert.key', {encoding: 'utf-8'})}"`))
+    .map(line => line.replace('cert-xxx', `${fs.readFileSync('.cert/cert.crt', {encoding: 'utf-8'})}`))
+    .map(line => line.replace('key-xxx', `${fs.readFileSync('.cert/cert.key', {encoding: 'utf-8'})}`))
     .join('\r\n');
 fs.writeFileSync('.startup.sh', lines, {encoding: 'utf-8'});
