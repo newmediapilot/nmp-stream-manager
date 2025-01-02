49c0ba6bea7d74a2acdef930a12bcee83eb40041
newmediapilot <marcin@newmediapilot.com>
2025-01-02 01:25:09 -0500

    Update proxy.js

--- src/proxy/proxy.js
+++ src/proxy/proxy.js
@@ -1,10 +1,11 @@
+const socketIo = require("socket.io");
 const express = require('express');
 const https = require('https');
 const app = express();
 app.all('/', (req, res) => {
     res.send(`Service online ${req.path}`);
 });
-https
+const httpsServer = https
     .createServer({
         key: `key-xxx`,
         cert: `cert-xxx`,
@@ -12,3 +13,10 @@ https
     .listen(443, () => {
         console.log('Server running');
     });
+const io = socketIo(httpsServer, {
+    path: '/demo/socket.io'
+});
+io.on("connection", (socket) => {
+    console.log("proxy :: client connected", socket.handshake.address);
+    socket.on("disconnect", () => console.log("proxy :: client disconnected"));
+});
