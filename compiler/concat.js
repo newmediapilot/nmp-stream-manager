66fdbb96e92e17b3be305caf9431188fc5a4be76
newmediapilot <marcin@newmediapilot.com>
2025-01-02 18:21:59 -0500

    Update concat.js

--- compiler/concat.js
+++ compiler/concat.js
@@ -38,6 +38,7 @@ const server = globSync('./src/server/**/*.*')
                     ...contentLines.map(line => line
                         .replace('module.exports =', 'return')
                         .replace(`/demo/`, `/${hash}/`)
+                        .replace(`io("https://192.168.0.22"`, `io("https://api.dbdbdbdbdbgroup.com"`)
                         .replace('baseURL = ""', 'baseURL = "https://api.dbdbdbdbdbgroup.com"')
                         .replace('process.env.TWITCH_CLIENT_ID', '"process.env.TWITCH_CLIENT_ID"')
                         .replace('process.env.TWITCH_CLIENT_SECRET', '"process.env.TWITCH_CLIENT_SECRET"')
