977afb3af0c31daa567943322cdc19eb4d64f613
newmediapilot <marcin@newmediapilot.com>
2025-01-02 01:43:57 -0500

    Update startup.sh

--- src/proxy/startup.sh
+++ src/proxy/startup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-sudo rm -rf /home/ec2-user/ && sudo mkdir /home/ec2-user/
-sudo echo '<h1>Instance is running and serving HTTP!</h1>' > index.html && sudo mv index.html /var/www/html/index.html
+set -e
 sudo yum update -y
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum install -y nodejs
-sudo mkdir -p /home/ec2-user/ && cd /home/ec2-user/
-sudo npm init -y
-sudo npm install express
-sudo npm install socket.io
-sudo tee proxy.js > /dev/null << 'EOF'
+sudo mkdir -p /home/ec2-user/
+sudo chown -R ec2-user:ec2-user /home/ec2-user/
+cd /home/ec2-user/
+su - ec2-user -c "npm init -y && npm install express socket.io"
+sudo tee /home/ec2-user/proxy.js > /dev/null << 'EOF'
 /** proxy.js **/
 EOF
-sudo node /home/ec2-user/proxy.js
+su - ec2-user -c "node /home/ec2-user/proxy.js"
