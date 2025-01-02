0c30c183c73b0dd5a6f40cc334f5852dfbfdfaad
newmediapilot <marcin@newmediapilot.com>
2025-01-02 00:27:48 -0500

    Update startup.sh

--- src/proxy/startup.sh
+++ src/proxy/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo mkdir -p  /var/www/html/
-sudo echo '<h1>Instance is running and serving HTTP!</h1>' > index.html
-sudo mv index.html /var/www/html/index.html
+sudo rm -rf /home/ec2-user/ && sudo mkdir /home/ec2-user/
+sudo echo '<h1>Instance is running and serving HTTP!</h1>' > index.html && sudo mv index.html /var/www/html/index.html
 sudo yum update -y
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
