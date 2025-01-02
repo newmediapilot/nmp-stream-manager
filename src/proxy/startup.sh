e148d71d52d7ceef3d4ba6613065effb27645c08
newmediapilot <marcin@newmediapilot.com>
2025-01-02 07:17:38 -0500

    Update startup.sh

--- src/proxy/startup.sh
+++ src/proxy/startup.sh
@@ -3,7 +3,8 @@ sudo yum update -y
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum install -y nodejs
-sudo mkdir -p /home/ec2-user/
+rm -rf /home/ec2-user/
+mkdir -p /home/ec2-user/
 cd /home/ec2-user/
 sudo npm init -y
 sudo npm install express
