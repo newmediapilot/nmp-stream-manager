dd51919bc4c5c1cfa88cb9c3244ac97a0194e253
newmediapilot <marcin@newmediapilot.com>
2025-01-02 20:13:39 -0500

    Update startup.sh

--- src/proxy/startup.sh
+++ src/proxy/startup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
+cd ~
 sudo yum update -y
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum install -y nodejs
-rm -rf /home/ec2-user/
-mkdir -p /home/ec2-user/
-sudo chown ec2-user:ec2-user /home/ec2-user/
-cd /home/ec2-user/
 sudo -u ec2-user npm init -y
 sudo -u ec2-user npm install express
 sudo -u ec2-user npm install socket.io
