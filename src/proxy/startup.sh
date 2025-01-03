deacefd41bbccdc52da440d0381bafdeb7f2e27e
newmediapilot <marcin@newmediapilot.com>
2025-01-02 21:29:26 -0500

    Update startup.sh

--- src/proxy/startup.sh
+++ src/proxy/startup.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
-
-# Change to the home directory
+rm -rf /var/lib/cloud/*
 cd /home/ec2-user
-
-# Update system packages
 sudo yum update -y
-
-# Install necessary build tools
 sudo yum install -y gcc-c++ make
-
-# Set up Node.js repository and install Node.js
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum install -y nodejs
-
-# Set permissions and create a clean directory for ec2-user
 sudo mkdir -p /home/ec2-user
 sudo chown ec2-user:ec2-user /home/ec2-user
-
-# Initialize a Node.js project and install dependencies as ec2-user
 sudo -u ec2-user bash -c "
   npm init -y
   npm install express socket.io
 "
-
-# Create proxy.js file
-cat > /home/ec2-user/proxy.js << 'EOF'
+cat >/home/ec2-user/proxy.js <<'EOF'
 /** proxy.js **/
 EOF
-
-# Ensure correct permissions for proxy.js
 sudo chown ec2-user:ec2-user /home/ec2-user/proxy.js
+sudo node /home/ec2-user/proxy.js &
