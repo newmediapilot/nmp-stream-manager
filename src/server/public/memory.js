c162f59a5e77a3a6a8343770b259578f7313689c
newmediapilot <marcin@newmediapilot.com>
2025-01-04 18:23:53 -0500

    Update memory.js

--- src/server/public/memory.js
+++ src/server/public/memory.js
@@ -1,16 +1,26 @@
 const https = require("https");
 const fetch = require("node-fetch");
+const {ROUTES} = require("../routes");
+const {publicSignalCreate} = require("../public/signal");
+const {publicConfigUpdate} = require("../public/config");
+const {publicStyleUpdate} = require("../public/style");
+const {publicMediaUpdate} = require("../public/media");
 const agent = new https.Agent({rejectUnauthorized: false});
 const getMemory = async () => {
     const response = await fetch("https://localhost/demo/api/memory/get", {agent});
     const memory = await response.json();
     for (let i = 0; i < memory.length; i++) {
-        const [method, url, body] = JSON.parse(memory[i]);
-        if ("GET" === method) await fetch(`https://localhost/demo/${url}`, {agent, method});
-        if ("POST" === method) await fetch(`https://localhost/demo/${url}`, {agent, method, body});
-        console.log('getMemory :: method', method);
-        console.log('getMemory :: url', url);
-        console.log('getMemory :: body', body);
+        const [method, url, path, query, params, body] = JSON.parse(memory[i]);
+        const req = {
+            method, url, path, query, params, body
+        };
+        const res = {
+            send: console.log
+        };
+        if (url.includes(ROUTES.API_SIGNAL_CREATE)) await publicSignalCreate(req, res);
+        if (url.includes(ROUTES.API_CONFIG_UPDATE)) await publicConfigUpdate(req, res);
+        if (url.includes(ROUTES.API_STYLE_UPDATE)) await publicStyleUpdate(req, res);
+        if (url.includes(ROUTES.API_MEDIA_UPDATE)) await publicMediaUpdate(req, res);
     }
 };
 const setMemory = async () => {
