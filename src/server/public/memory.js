74d1eba73b40b02bbd4446f70f7f5d8c43f609cd
newmediapilot <marcin@newmediapilot.com>
2025-01-04 02:20:29 -0500

    Update memory.js

--- src/server/public/memory.js
+++ src/server/public/memory.js
@@ -4,10 +4,7 @@ const agent = new https.Agent({rejectUnauthorized: false});
 const getMemory = async () => {
     const response = await fetch("https://localhost/demo/api/memory/get", {agent});
     const memory = await response.json();
-    for (let i = 0; i < memory.length; i++) {
-        // await write to self
-        console.log('getMemory ::', memory[i]);
-    }
+    console.log('memory', memory);
 };
 const setMemory = async () => {
     const response = await fetch("https://localhost/demo/api/memory/set", {agent});
