3939656ed6d4b5f0ede826ae4dafa333e487ff41
newmediapilot <marcin@newmediapilot.com>
2025-01-04 14:58:50 -0500

    Update memory.js

--- src/server/public/memory.js
+++ src/server/public/memory.js
@@ -1,12 +1,24 @@
 const https = require("https");
 const fetch = require("node-fetch");
+const {ROUTES} = require("../routes");
 const agent = new https.Agent({rejectUnauthorized: false});
 const getMemory = async () => {
     const response = await fetch("https://localhost/demo/api/memory/get", {agent});
     const memory = await response.json();
-    console.log('memory', memory);
+    for (let i = 0; i < memory.length; i++) {
+        const [path, type, description, payload] = JSON.parse(memory[i]);
+        const url = path.split('/').slice(1);
+        if (url.endsWith(ROUTES.API_MEDIA_UPDATE)) {
+            await fetch(path, {method: "post"});
+        } else {
+            await fetch(path, {method: "get"});
+        }
+        console.log('memory :: mem ::', path, type, description, payload);
+    }
 };
 const setMemory = async () => {
     const response = await fetch("https://localhost/demo/api/memory/set", {agent});
+    // TODO: send style
+    // TODO: send config
 };
 module.exports = {getMemory, setMemory};
