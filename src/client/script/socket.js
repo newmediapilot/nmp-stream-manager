7b63ae1404aad59897946d6ff93cf713b1a1bb96
newmediapilot <marcin@newmediapilot.com>
2025-01-04 01:36:43 -0500

    Update socket.js

--- src/client/script/socket.js
+++ src/client/script/socket.js
@@ -1,6 +1,6 @@
 const socketConnect = () => {
     if (document.$socketIO) return;
-    document.$socketIO = io("https://192.168.0.22", {
+    document.$socketIO = io("https://localhost", {
         path: '/demo/socket.io',
     });
     document.$socketIO.on("connect_error", (err) =>
