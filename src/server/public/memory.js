e2e7ed91abdcc69d933f8b2dba346939c8d0d7cf
newmediapilot <marcin@newmediapilot.com>
2025-01-04 23:27:01 -0500

    Update memory.js

--- src/server/public/memory.js
+++ src/server/public/memory.js
@@ -1,31 +1,33 @@
 const https = require("https");
 const fetch = require("node-fetch");
 const {ROUTES} = require("../routes");
-const {publicSignalCreate} = require("../public/signal");
-const {publicConfigUpdate} = require("../public/config");
-const {publicStyleUpdate} = require("../public/style");
-const {publicMediaUpdate} = require("../public/media");
 const agent = new https.Agent({rejectUnauthorized: false});
 const getMemory = async () => {
-    const response = await fetch("https://localhost/demo/api/memory/get", {agent});
-    const memory = await response.json();
-    for (let i = 0; i < memory.length; i++) {
-        const [method, url, path, query, params, body] = JSON.parse(memory[i]);
-        const req = {
-            method, url, path, query, params, body
-        };
-        const res = {
-            send: console.log
-        };
-        if (url.includes(ROUTES.API_SIGNAL_CREATE)) await publicSignalCreate(req, res);
-        if (url.includes(ROUTES.API_CONFIG_UPDATE)) await publicConfigUpdate(req, res);
-        if (url.includes(ROUTES.API_STYLE_UPDATE)) await publicStyleUpdate(req, res);
-        if (url.includes(ROUTES.API_MEDIA_UPDATE)) await publicMediaUpdate(req, res);
+    try {
+        let result;
+        const response = await fetch("https://localhost/demo/api/memory/get", {agent});
+        const memory = await response.json();
+        for (let i = 0; i < memory.length; i++) {
+            const [method, url, query, body] = JSON.parse(memory[i]);
+            if (url.includes(ROUTES.API_SIGNAL_CREATE)) {
+                result = await fetch(`${ROUTES.API_SIGNAL_CREATE}?type=${query.type}&description=${query.description}`, {agent,method});
+            }
+            if (url.includes(ROUTES.API_CONFIG_UPDATE)) {
+                result = await fetch(`${ROUTES.API_CONFIG_UPDATE}?type=${query.type}&description=${query.description}`, {agent,method});
+            }
+            if (url.includes(ROUTES.API_STYLE_UPDATE)) {
+                result = await fetch(`${ROUTES.API_STYLE_UPDATE}?type=${query.type}&description=${query.description}`, {agent,method});
+            }
+            if (url.includes(ROUTES.API_MEDIA_UPDATE)) {
+                result = await fetch(`${ROUTES.API_STYLE_UPDATE}?type=${query.type}&description=${query.description}`, {agent,method, body});
+            }
+            console.log('memory :: fetch', url, result);
+        }
+    } catch (error) {
+        console.log('memory :: error', error)
     }
 };
 const setMemory = async () => {
     const response = await fetch("https://localhost/demo/api/memory/set", {agent});
-    // TODO: send style
-    // TODO: send config
 };
 module.exports = {getMemory, setMemory};
