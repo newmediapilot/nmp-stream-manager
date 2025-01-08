fba4e7be91e9bafc6da3e8fbd4a14e7ac756b9b5
newmediapilot <marcin@newmediapilot.com>
2025-01-08 12:07:30 -0500

    Update config.js

--- src/server/public/config.js
+++ src/server/public/config.js
@@ -85,47 +85,40 @@ const path = require("path");
 const {sendConfig} = require("../public/memory");
 const {setParam, getParam} = require("../store/manager");
 const initializePublicConfigs = async (type) => {
-    console.log( "initializePublicConfigs :: start");
+    console.log("initializePublicConfigs :: start");
     const fileName = path.resolve(`.${type}`);
     try {
         if (!fs.existsSync(fileName)) {
             setParam("dashboard_signals_config", DASHBOARD_BUTTON_CONFIG.signals);
             putConfig("signals", DASHBOARD_BUTTON_CONFIG.signals);
-            console.log( "initializePublicConfigs :: new file");
+            console.log("initializePublicConfigs :: new file");
         } else {
             setParam("dashboard_signals_config", getConfig("signals"));
-            console.log( "initializePublicConfigs :: load file");
+            console.log("initializePublicConfigs :: load file");
         }
-        console.log( "initializePublicConfigs :: success");
+        console.log("initializePublicConfigs :: success");
         return true;
     } catch (e) {
-        console.log( "initializePublicConfigs :: error", e);
+        console.log("initializePublicConfigs :: error", e);
         return false;
     } finally {
-        console.log( "initializePublicConfigs :: done");
+        console.log("initializePublicConfigs :: done");
     }
 };
 const putConfig = (filePath, config) => {
     const fileName = path.resolve(`${filePath}`);
-    console.log(
-        "putConfig :: file:",
-        fileName,
-        ":: contents :",
-        config.map((c) => (c.label || 'empty')),
-    );
+    console.log("putConfig :: file:", fileName, ":: contents :", config.map((c) => (c.label || 'empty')));
     sendConfig();
     fs.writeFileSync(fileName, JSON.stringify(config), {encoding: "utf-8"});
 };
 const getConfig = (type) => {
     const fileName = path.resolve(`.${type}`);
-    console.log( "getConfig :: file:", fileName);
+    console.log("getConfig :: file:", fileName);
     return JSON.parse(fs.readFileSync(fileName, "utf-8"));
 };
 const applySignalsOrder = (payloadJSON) => {
     const payloadAction = payloadJSON ? JSON.parse(payloadJSON) : [];
-    const signalsTarget = JSON.parse(
-        JSON.stringify(getParam("dashboard_signals_config")),
-    );
+    const signalsTarget = JSON.parse(JSON.stringify(getParam("dashboard_signals_config")));
     Array.from({length: payloadAction.length / 2}).map(() => {
         const [from, to] = payloadAction.splice(0, 2);
         const [fromEl, toEl] = [signalsTarget[from], signalsTarget[to]];
@@ -136,10 +129,8 @@ const applySignalsOrder = (payloadJSON) => {
     return signalsTarget;
 };
 const applySignalsField = (payloadJSON) => {
-    console.log( "applySignalsField", payloadJSON);
-    const signalsTarget = JSON.parse(
-        JSON.stringify(getParam("dashboard_signals_config")),
-    );
+    console.log("applySignalsField", payloadJSON);
+    const signalsTarget = JSON.parse(JSON.stringify(getParam("dashboard_signals_config")));
     const {id, field, value} = JSON.parse(payloadJSON);
     signalsTarget[Number(id)][field] = value;
     setParam("dashboard_signals_config", signalsTarget);
@@ -147,30 +138,21 @@ const applySignalsField = (payloadJSON) => {
 };
 const publicConfigUpdate = (req, res) => {
     const {type, payload} = req.query;
-    console.log( "publicConfigUpdate", type, payload);
+    console.log("publicConfigUpdate", type, payload);
     try {
-        if (type === "signals:order") {
-            putConfig("signals", applySignalsOrder(payload));
-        }
-        if (type === "signals:field") {
-            putConfig("signals", applySignalsField(payload));
-        }
-        res.status(200).json({
-            message: "Configuration for " + type + " updated successfully.",
-        });
+        if (type === "signals:order") putConfig("signals", applySignalsOrder(payload));
+        if (type === "signals:field") putConfig("signals", applySignalsField(payload));
+        res.status(200).json({message: "Configuration for " + type + " updated successfully."});
     } catch (error) {
-        console.log(
-            "Error processing configuration:",
-            error.message,
-        );
+        console.log("publicConfigUpdate :: error processing configuration:", error.message);
         res.status(500).json({error: error.message});
     }
 };
 const configFieldUpdate = (id, field, value) => {
-    console.log( "configFieldUpdate", id, field, value);
+    console.log("publicConfigUpdate ::", id, field, value);
     const signalsTarget = JSON.parse(JSON.stringify(getParam("dashboard_signals_config")));
     signalsTarget[Number(id)][field] = value;
     setParam("dashboard_signals_config", signalsTarget);
     putConfig("signals", signalsTarget);
 };
-module.exports = { initializePublicConfigs, publicConfigUpdate, configFieldUpdate};
+module.exports = {initializePublicConfigs, publicConfigUpdate, configFieldUpdate};
