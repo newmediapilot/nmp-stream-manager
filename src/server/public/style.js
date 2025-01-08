c136ef3d9b0b7a83ed1740a22ed33e9cb63a1958
newmediapilot <marcin@newmediapilot.com>
2025-01-08 11:56:25 -0500

    Update style.js

--- src/server/public/style.js
+++ src/server/public/style.js
@@ -2,39 +2,41 @@ const fs = require("fs");
 const path = require("path");
 const {setParam} = require("../store/manager");
 const initializePublicStyles = async (type) => {
-    console.log( "initializePublicStyles :: start");
+    console.log("initializePublicStyles :: start");
     try {
         if (!fs.existsSync(path.resolve(`${type}`))) {
-            console.log( "initializePublicStyles :: new file");
+            console.log("initializePublicStyles :: new file");
             setParam("public_module_styles", '/*--_empty:1;*/');
             fs.writeFileSync(path.resolve(`${type}`), '/*--_empty:1;*/', {encoding: "utf-8"});
         } else {
             setParam("public_module_styles",
                 fs.readFileSync(path.resolve(`${type}`), "utf-8")
             );
-            console.log( "initializePublicStyles :: load file");
+            console.log("initializePublicStyles :: load file");
         }
-        console.log( "initializePublicStyles :: success");
+        sendStyle();
+        console.log("initializePublicStyles :: success");
         return true;
     } catch (e) {
-        console.log( "initializePublicStyles :: error", e);
+        console.log("initializePublicStyles :: error", e);
         return false;
     } finally {
-        console.log( "initializePublicStyles :: done");
+        console.log("initializePublicStyles :: done");
     }
 };
 // TODO: add validation to prevent non-variables from entering
 const publicStyleUpdate = (req, res) => {
     try {
         const {type, payload} = req.query;
-        console.log( "publicStyleUpdate", type, payload);
+        console.log("publicStyleUpdate", type, payload);
         if ("style" === type) {
-            fs.writeFileSync(path.resolve(`${type}`), payload, {encoding: "utf-8"});
             setParam("public_module_styles", payload);
-            console.log( "publicStyleUpdate :: style", payload);
+            sendStyle();
+            fs.writeFileSync(path.resolve(`${type}`), payload, {encoding: "utf-8"});
+            console.log("publicStyleUpdate :: style", payload);
         }
         res.status(200).json({
-            message: "Style for " + type + " updated successfully.",
+            message: "publicStyleUpdate :: style for " + type + " updated successfully.",
         });
     } catch (error) {
         console.log(
@@ -43,7 +45,7 @@ const publicStyleUpdate = (req, res) => {
         );
         res.status(500).json({error: error.message});
     } finally {
-        console.log( "Done processing style");
+        console.log("publicStyleUpdate :: done processing style");
     }
 };
 module.exports = {initializePublicStyles, publicStyleUpdate};
