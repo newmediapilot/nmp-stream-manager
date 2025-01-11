9f57d9c17a41889624dc1301c099a4bf92704e9c
newmediapilot <marcin@newmediapilot.com>
2025-01-10 20:09:00 -0500

    Update routes.js

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,8 +1,8 @@
 const ROUTES = {
-    MEMORY_SIGNAL_CREATE: "/memory/signal/create",
-    MEMORY_CONFIG_UPDATE: "/memory/config/update",
-    MEMORY_STYLE_UPDATE: "/memory/style/update",
-    MEMORY_MEDIA_UPDATE: "/memory/media/update",
+    MEMORY_SIGNAL_CREATE: "/demo/memory/signal/create",
+    MEMORY_CONFIG_UPDATE: "/demo/memory/config/update",
+    MEMORY_STYLE_UPDATE: "/demo/memory/style/update",
+    MEMORY_MEDIA_UPDATE: "/demo/memory/media/update",
     API_SIGNAL_CREATE: "/api/signal/create",
     API_CONFIG_UPDATE: "/api/config/update",
     API_STYLE_UPDATE: "/api/style/update",
