d69c8e2dcad0ec71b7f7a60f03ef14c31908ee3c
newmediapilot <marcin@newmediapilot.com>
2025-01-23 19:33:22 -0500

    Update concat.js

--- compiler/concat.js
+++ compiler/concat.js
@@ -242,7 +242,8 @@ const templates = globSync('./src/templates/**/*.*')
                     line = line.replace(new RegExp('content="/demo/embed-sound.html', "gm"), `content="https://dbdbdbdbdbgroup.com/${hash}/embed-sound.html`);
                     line = line.replace(new RegExp('content="/demo/t/w/i/t/c/h/l/o/g/i/n/', "gm"), `content="https://api.dbdbdbdbdbgroup.com/${hash}/t/w/i/t/c/h/l/o/g/i/n/`);
                     line = line.replace(new RegExp('content="/t/w/i/t/c/h/l/o/g/i/n/s/u/c/c/e/s/s/', "gm"), `content="https://api.dbdbdbdbdbgroup.com/t/w/i/t/c/h/l/o/g/i/n/s/u/c/c/e/s/s/`);
-                    line = line.replace(new RegExp('https://dbdbdbdbdbgroup.com/hello.html\?', "gm"), `https://dbdbdbdbdbgroup.com/${hash}/`);
+                    line = line.replace(new RegExp('data-url="{{public_routes.INDEX}}"', "gm"), `data-url="https://dbdbdbdbdbgroup.com/${hash}/index.html"`);
+                    line = line.replace(new RegExp('data-url="{{public_routes.PANEL_EMBED}}"', "gm"), `data-url="https://dbdbdbdbdbgroup.com/${hash}/embed.html"`);
                     line = line.replace(new RegExp('https://api.dbdbdbdbdbgroup.com/demo/', "gm"), `https://api.dbdbdbdbdbgroup.com/${hash}/`);
                     return line;
                 });
