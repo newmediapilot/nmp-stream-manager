a5329485ce1a49b739f5a58ccfa20bc2bf22a666
newmediapilot <marcin@newmediapilot.com>
2025-01-25 17:57:54 -0500

    Update manifest.js

--- compiler/manifest.js
+++ compiler/manifest.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const {sync: globSync} = require('glob');
 const hash = process.argv[2] || 'demo';
-const manifestJSON = JSON.parse(fs.readFileSync(globSync("src/client/manifest.json")[0], {encoding: "utf-8"}));
+const manifestJSON = JSON.parse(fs.readFileSync(globSync("src/client/demo/manifest.json")[0], {encoding: "utf-8"}));
 manifestJSON.start_url = `/${hash}/start.html`;
 fs.writeFileSync(
     'manifest.json',
