792834654a71f8e7f5650ceb0a4a2e6b5151fb1c
newmediapilot <marcin@newmediapilot.com>
2025-01-27 17:53:59 -0500

    remove start.html

--- compiler/manifest.js
+++ compiler/manifest.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const {sync: globSync} = require('glob');
 const hash = process.argv[2] || 'demo';
 const manifestJSON = JSON.parse(fs.readFileSync(globSync("src/client/demo/manifest.json")[0], {encoding: "utf-8"}));
-manifestJSON.start_url = `/${hash}/start.html`;
+manifestJSON.start_url = `/${hash}/index.html`;
 fs.writeFileSync(
     'manifest.json',
     JSON.stringify(manifestJSON),
