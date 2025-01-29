d2942c03b8d7d9670939aa06985427a244898b9f
newmediapilot <marcin@newmediapilot.com>
2025-01-29 02:45:37 -0500

    Update routes.js

--- src/server/routes.js
+++ src/server/routes.js
@@ -17,7 +17,7 @@ const ROUTES = {
     PANEL_MEDIA_EMBED: "/demo/embed-media.html",
     PANEL_DRAW_EMBED: "/demo/embed-draw.html",
     PANEL_SOUND_EMBED: "/demo/embed-sound.html",
-    TWITCH_LOGIN: "/demo/t/w/i/t/c/h/l/o/g/i/n/",
+    TWITCH_LOGIN: "/t/w/i/t/c/h/l/o/g/i/n/",
     TWITCH_LOGIN_SUCCESS: "/t/w/i/t/c/h/l/o/g/i/n/s/u/c/c/e/s/s/",
 };
 module.exports = {ROUTES};
