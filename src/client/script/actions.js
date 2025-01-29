7409244f01e36dac033f0d26bb2e2d0593864a0c
newmediapilot <marcin@newmediapilot.com>
2025-01-28 20:47:55 -0500

    Update actions.js

--- src/client/script/actions.js
+++ src/client/script/actions.js
@@ -179,6 +179,12 @@ const actionsCreateUpload = (editorEl) => {
             "image" === type && displayMedia();
         });
     }
+    socketWatchSync(()=>{
+        console.log('socketWatchSync :: toggleReplayState');
+        if (!!actionButton) {
+            toggleReplayState();
+        }
+    });
 };
 
 const actionsToggle = (editorEl) => {
