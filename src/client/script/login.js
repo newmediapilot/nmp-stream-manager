1f97206434a419f78e1845ed7c97b62197a9418e
newmediapilot <marcin@newmediapilot.com>
2025-01-31 21:34:23 -0500

    Create login.js

---
+++ src/client/script/login.js
@@ -0,0 +1,15 @@
+function login() {
+    const dialogEl = document.querySelector('#login-dialog');
+    const buttonEl = dialogEl.querySelector('button');
+    const inputEl = dialogEl.querySelector('input[type=text]');
+    buttonEl.addEventListener('click', () => {
+        if (inputEl.value.length > 1) {
+            const url = `${getPath('TWITCH_LOGIN')}?username=${inputEl.value}`;
+            console.log('url', url);
+        }
+    });
+    inputEl.addEventListener('change', () => {
+        buttonEl.disabled = (inputEl.value.length === 0);
+    });
+    buttonEl.disabled = true;
+}
