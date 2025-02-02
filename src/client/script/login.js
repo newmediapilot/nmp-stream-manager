b355ebe6468e9f3914e6645e05ad901ca6217b26
newmediapilot <marcin@newmediapilot.com>
2025-02-01 20:21:53 -0500

    Update login.js

--- src/client/script/login.js
+++ src/client/script/login.js
@@ -2,9 +2,11 @@ function login() {
     const dialogEl = document.querySelector('#login-dialog');
     const buttonEl = dialogEl.querySelector('button');
     const inputEl = dialogEl.querySelector('input[type=text]');
+    inputEl.value = localStorage.getItem("username") || "";
     buttonEl.addEventListener('click', () => {
         if (inputEl.value.length > 1) {
             const href = `${getPath('TWITCH_LOGIN')}?username=${inputEl.value}`;
+            localStorage.setItem("username", inputEl.value);
             window.location.href = href;
         }
     });
