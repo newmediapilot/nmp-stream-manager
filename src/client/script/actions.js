352414daace9c29cb08c4e0c54a35fbbac493674
newmediapilot <marcin@newmediapilot.com>
2025-02-12 19:51:37 -0500

    Update actions.js

--- src/client/script/actions.js
+++ src/client/script/actions.js
@@ -38,6 +38,7 @@ const actionsCreateEmojis = (editorEl) => {
                         },
                     }).finally(() => {
                         socketEmitReload();
+                        emojiWidgetTriggerEl.click();
                     })
                 });
             });
