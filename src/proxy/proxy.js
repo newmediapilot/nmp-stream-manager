deee32f689273896a9d2084bea347b0491873dcd
newmediapilot <marcin@newmediapilot.com>
2025-02-19 20:15:22 -0500

    Update proxy.js

--- src/proxy/proxy.js
+++ src/proxy/proxy.js
@@ -158,13 +158,7 @@ const memorize = (req, key) => {
         memorize(req, key);
     });
     app.all(`/${key}${ROUTES.API_SIGNAL_CREATE}`, (req, res) => {
-        const hash = hashify(req.ip, key);
-        if ("style" === req.query.type) {
-            sockets[hash].to('dbdbdbdbdbgroup').emit('payload', `style:set:${req.query.description}`);
-            console.log(`proxy :: API_SIGNAL_CREATE :: style ${req.query.description.length}`);
-        } else {
-            memorize(req, key);
-        }
+        memorize(req, key);
         res.send(`200 @ ${time}`);
     });
     [
